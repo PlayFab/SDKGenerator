e7a4af82f6190df2543988f5c26e5d113b7addab
Jeremy Friesen <jeremy@playfab.com>
2015-06-08 14:48:07 -0700

    Adding Unity plugin, manager and iOS HTTP workaround

--- targets/csharp-unity/source/Playfab/PlayFabSDK/Internal/PlayFabHTTP.cs
+++ targets/csharp-unity/source/Playfab/PlayFabSDK/Internal/PlayFabHTTP.cs
@@ -1,4 +1,13 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+
+#elif UNITY_ANDROID
+#define PLAYFAB_ANDROID_PLUGIN
+#elif UNITY_IOS
+#define PLAYFAB_IOS_PLUGIN
+#endif
+
+
+using UnityEngine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,13 +17,17 @@ namespace PlayFab.Internal
 {
 	public class PlayFabHTTP : SingletonMonoBehaviour<PlayFabHTTP>  {
 		
-		
+
 		/// <summary>
 		/// Sends a POST HTTP request
 		/// </summary>
 		public static void Post (string url, string data, string authType, string authKey, Action<string,string> callback)
 		{
+#if PLAYFAB_IOS_PLUGIN
+			PlayFabiOSPlugin.Post(url, data, authType, authKey, PlayFabVersion.getVersionString(), callback);
+#else
 			PlayFabHTTP.instance.InstPost(url, data, authType, authKey, callback);
+#endif
 		}
 		
 		private void InstPost (string url, string data, string authType, string authKey, Action<string,string> callback)
@@ -25,7 +38,6 @@ namespace PlayFab.Internal
 		private IEnumerator MakeRequest (string url, string data, string authType, string authKey, Action<string,string> callback)
 		{
 			byte[] bData = System.Text.Encoding.UTF8.GetBytes(data);
-
 
 #if UNITY_4_4 || UNITY_4_3 || UNITY_4_2 || UNITY_4_2 || UNITY_4_0 || UNITY_3_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5
 			// Using hashtable for compatibility with Unity < 4.5
@@ -38,7 +50,6 @@ namespace PlayFab.Internal
 				headers.Add(authType, authKey);
 			headers.Add("X-ReportErrorAsSuccess", "true");
 			headers.Add("X-PlayFabSDK", PlayFabVersion.getVersionString ());
-
 			WWW www = new WWW(url, bData, headers);
 			yield return www;
 			

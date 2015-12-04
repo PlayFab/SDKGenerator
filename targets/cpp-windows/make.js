15bf30b50eb92abb0e9de610a203e536e64287d5
pgilmorepf <paul@playfab.com>
2015-12-04 12:55:12 -0800

    Win: More cleanup and settings variables.

--- targets/cpp-windows/make.js
+++ targets/cpp-windows/make.js
@@ -2,84 +2,60 @@ var path = require("path");
 var shared = require("../cpp-shared/cpp-shared");
 
 exports.makeClientAPI = function (api, sourceDir, apiOutputDir) {
-    var libname = "Client";
-    
     console.log("Generating Windows C++ client SDK to " + apiOutputDir);
     
     copyTree(path.resolve(sourceDir, "../cpp-shared/source"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "source"), apiOutputDir);
     
     shared.makeAPI(api, apiOutputDir, "core/");
-    
-    shared.generateModels([api], apiOutputDir, libname, "core/");
-    
+    shared.generateModels([api], apiOutputDir, "Client", "core/");
     shared.generateErrors(api, apiOutputDir);
     generateSimpleFiles([api], sourceDir, apiOutputDir);
-    
-    makeAPIProject([api], sourceDir, apiOutputDir, libname);
+    makeAPIProject([api], sourceDir, apiOutputDir, "Client");
 }
 
 exports.makeServerAPI = function (apis, sourceDir, apiOutputDir) {
-    var libname = "Server";
-    
     console.log("Generating Windows C++ server SDK to " + apiOutputDir);
     
     copyTree(path.resolve(sourceDir, "../cpp-shared/source"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "source"), apiOutputDir);
     
-    for (var i in apis) {
-        var api = apis[i];
-        
-        shared.makeAPI(api, apiOutputDir, "core/");
-    }
-    
-    shared.generateModels(apis, apiOutputDir, libname, "core/");
-    
+    for (var i in apis)
+        shared.makeAPI(apis[i], apiOutputDir, "core/");
+    shared.generateModels(apis, apiOutputDir, "Server", "core/");
     shared.generateErrors(apis[0], apiOutputDir);
     generateSimpleFiles(apis, sourceDir, apiOutputDir);
-    
-    makeAPIProject(apis, sourceDir, apiOutputDir, libname);
+    makeAPIProject(apis, sourceDir, apiOutputDir, "Server");
 }
 
 exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
-    var libname = "All";
-    
     console.log("Generating Windows C++ combined SDK to " + apiOutputDir);
     
     copyTree(path.resolve(sourceDir, "../cpp-shared/source"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "source"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "UnittestRunner"), path.resolve(apiOutputDir, "build/VC12/UnittestRunner"));
     
-    for (var i in apis) {
-        var api = apis[i];
-        
-        shared.makeAPI(api, apiOutputDir, "core/");
-    }
-    
-    shared.generateModels(apis, apiOutputDir, libname, "core/");
-    
+    for (var i in apis)
+        shared.makeAPI(apis[i], apiOutputDir, "core/");
+    shared.generateModels(apis, apiOutputDir, "All", "core/");
     shared.generateErrors(apis[0], apiOutputDir);
     generateSimpleFiles(apis, sourceDir, apiOutputDir);
-    
-    makeAPIProject(apis, sourceDir, apiOutputDir, libname);
+    makeAPIProject(apis, sourceDir, apiOutputDir, "All");
 }
 
-
 function makeAPIProject(apis, sourceDir, apiOutputDir, libname) {
-    var vcProjTemplate = ejs.compile(readFile(path.resolve(sourceDir, "templates/PlayFabAPI.vcxproj.ejs")));
-    var vcProjFilterTemplate = ejs.compile(readFile(path.resolve(sourceDir, "templates/PlayFabAPI.vcxproj.filters.ejs")));
-    
     var projLocals = {};
     projLocals.apis = apis;
     projLocals.libname = libname;
     
+    var vcProjTemplate = ejs.compile(readFile(path.resolve(sourceDir, "templates/PlayFabAPI.vcxproj.ejs")));
     var generatedProject = vcProjTemplate(projLocals);
     writeFile(path.resolve(apiOutputDir, "build/VC12/PlayFabAPI/PlayFabAPI.vcxproj"), generatedProject);
     
+    var vcProjFilterTemplate = ejs.compile(readFile(path.resolve(sourceDir, "templates/PlayFabAPI.vcxproj.filters.ejs")));
     var generatedFilters = vcProjFilterTemplate(projLocals);
     writeFile(path.resolve(apiOutputDir, "build/VC12/PlayFabAPI/PlayFabAPI.vcxproj.filters"), generatedFilters);
 }
-
 
 function generateSimpleFiles(apis, sourceDir, apiOutputDir) {
     var versionTemplate = ejs.compile(readFile(path.resolve(sourceDir, "templates/PlayFabVersion.cpp.ejs")));

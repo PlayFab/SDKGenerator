7f200e4993cf1a9a4dc66f229ba681597e867ad5
Tim Graupmann <tim@tagenigma.com>
2016-02-25 23:41:07 -0800

    Expanded scripting unit test

--- targets/cpp-unreal/ExampleProject/Source/ExampleProject/PlayFabUnitTest.cpp
+++ targets/cpp-unreal/ExampleProject/Source/ExampleProject/PlayFabUnitTest.cpp
@@ -1,22 +1,42 @@
 #include "ExampleProject.h"
 
 #include "PlayFabUnitTest.h"
-#include "PlayFabClientAPI.h"
-#include "PlayFabClientModels.h"
 
 UPlayFabUnitTest::UPlayFabUnitTest(const FObjectInitializer& ObjectInitializer)
     : Super(ObjectInitializer)
 {
+	_mClientAPI = NULL;
 }
 
-UPlayFabClientAPI* UPlayFabUnitTest::UnitTestRegisterUser()
+void UPlayFabUnitTest::Activate()
+{
+	if (_mClientAPI)
+	{
+		_mClientAPI->Activate();
+	}
+}
+
+UPlayFabUnitTest* UPlayFabUnitTest::UnitTestRegisterUser()
 {
 	FClientRegisterPlayFabUserRequest request;
 	request.Username = "myUsername";
 	request.Email = "email@email.com";
 	request.Username = "password";
+	UPlayFabUnitTest* proxy = NewObject<UPlayFabUnitTest>();
 	UPlayFabClientAPI::FDelegateOnSuccessRegisterPlayFabUser onSuccess;
+	onSuccess.BindUFunction(proxy, TEXT("OnSuccessUnitTestRegisterUser"));
 	UPlayFabClientAPI::FDelegateOnFailurePlayFabError onFailure;
-	UPlayFabClientAPI* manager = UPlayFabClientAPI::RegisterPlayFabUser(request, onSuccess, onFailure);
-	return manager;
+	onFailure.BindUFunction(proxy, TEXT("OnFailureUnitTestRegisterUser"));
+	proxy->_mClientAPI = UPlayFabClientAPI::RegisterPlayFabUser(request, onSuccess, onFailure);
+	return proxy;
+}
+
+void UPlayFabUnitTest::OnSuccessUnitTestRegisterUser(FClientRegisterPlayFabUserResult result)
+{
+	UE_LOG(LogTemp, Log, TEXT("UPlayFabUnitTest::OnSuccessUnitTestRegisterUser"));
+}
+
+void UPlayFabUnitTest::OnFailureUnitTestRegisterUser(FPlayFabError error)
+{
+	UE_LOG(LogTemp, Log, TEXT("UPlayFabUnitTest::OnFailureUnitTestRegisterUser"));
 }
--- targets/cpp-unreal/ExampleProject/Source/ExampleProject/PlayFabUnitTest.h
+++ targets/cpp-unreal/ExampleProject/Source/ExampleProject/PlayFabUnitTest.h
@@ -2,15 +2,34 @@
 
 class UPlayFabClientAPI;
 
+#include "OnlineBlueprintCallProxyBase.h"
+#include "PlayFabClientAPI.h"
+#include "PlayFabClientModels.h"
 #include "PlayFabUnitTest.generated.h"
 
 UCLASS(Blueprintable, BlueprintType)
-class UPlayFabUnitTest : public UObject
+class UPlayFabUnitTest : public UOnlineBlueprintCallProxyBase
 {
 	GENERATED_UCLASS_BODY()
 
 public:
 
-	UFUNCTION(BlueprintCallable, Category = "PlayFab | Client | Authentication ")
-	static UPlayFabClientAPI* UnitTestRegisterUser();
+	UPROPERTY(BlueprintAssignable)
+		FOnPlayFabClientRequestCompleted OnPlayFabResponse;
+
+	/** UOnlineBlueprintCallProxyBase interface */
+	virtual void Activate() override;
+
+	UFUNCTION(BlueprintCallable, Category = "PlayFab | Client | Authentication ", meta = (BlueprintInternalUseOnly = "true"))
+		static UPlayFabUnitTest* UnitTestRegisterUser();
+
+	UFUNCTION(BlueprintCallable, Category = "PlayFab | Client | Authentication ", meta = (BlueprintInternalUseOnly = "true"))
+	void OnSuccessUnitTestRegisterUser(FClientRegisterPlayFabUserResult result);
+
+	UFUNCTION(BlueprintCallable, Category = "PlayFab | Client | Authentication ", meta = (BlueprintInternalUseOnly = "true"))
+	void OnFailureUnitTestRegisterUser(FPlayFabError error);
+
+private:
+
+	UPlayFabClientAPI* _mClientAPI;
 };

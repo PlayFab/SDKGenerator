31d70361d057be8f7f59e4a78e7b263e64d0bc4e
pgilmorepf <paul@playfab.com>
2016-04-28 15:01:26 -0700

    Update the examples to use ExecuteCloudScript and login with customID.
    Summary:
    Improving the error response for UnrealBP.
    
    ErrorDetails is now reported to the user, and you get better error reports in general.
    
    Test Plan: manual testing, but slightly refined and improved
    
    Reviewers: #devtools_in-progress, zac-playfab
    
    Reviewed By: #devtools_in-progress, zac-playfab
    
    Subscribers: zac-playfab
    
    Maniphest Tasks: T1301
    
    Differential Revision: https://phab.playfabdev.com/D1235

--- targets/cpp-unreal/Source/PlayFab/Private/PlayFabBaseModel.cpp
+++ targets/cpp-unreal/Source/PlayFab/Private/PlayFabBaseModel.cpp
@@ -7,7 +7,6 @@
 
 void FPlayFabError::decodeError(UPlayFabJsonObject* responseData)
 {
-
     // Check if we have an error
     if (int(responseData->GetNumberField("code")) != 200) // We have an error
     {
@@ -15,8 +14,22 @@ void FPlayFabError::decodeError(UPlayFabJsonObject* responseData)
         ErrorCode = int(responseData->GetNumberField("errorCode"));
         ErrorName = responseData->GetStringField("error");
         ErrorMessage = responseData->GetStringField("errorMessage");
-        ErrorDetails = responseData->GetStringField("errorDetails");
+        auto detailsObj = responseData->GetObjectField("errorDetails");
+        ErrorDetails = "";
+        int count = 0;
+        for (auto detailParamPair = detailsObj->GetRootObject()->Values.CreateConstIterator(); detailParamPair; ++detailParamPair)
+        {
+            auto errorArray = detailParamPair->Value->AsArray();
+            for (auto paramMsg = errorArray.CreateConstIterator(); paramMsg; ++paramMsg)
+            {
+                if (count != 0)
+                    ErrorDetails += "\n";
+                ErrorDetails += detailParamPair->Key;
+                ErrorDetails += ": ";
+                ErrorDetails += paramMsg->Get()->AsString();
+                count++;
+            }
+        }
     }
     else { hasError = false; }
-
 }

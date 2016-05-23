30238188ad706351dcfe7c5de8373cfb0872ad18
pgilmorepf <paul@playfab.com>
2016-05-23 10:33:44 -0700

    Build configurations in VS
    Summary: Build configurations in VS
    
    Test Plan: Nothin really
    
    Reviewers: #devtools_in-progress, zac-playfab
    
    Reviewed By: #devtools_in-progress, zac-playfab
    
    Subscribers: zac-playfab
    
    Differential Revision: https://phab.playfabdev.com/D1406

--- SDKBuildScripts/js_build.bat
+++ SDKBuildScripts/js_build.bat
@@ -1,9 +1,9 @@
 pushd ..
 if [%1] == [] (
 rem === BUILDING JavascriptSDK ===
-node generate.js ..\API_Specs javascript=..\sdks\JavascriptSDK\PlayFabSDK
+node generate.js ..\API_Specs javascript=..\sdks\JavaScriptSDK\PlayFabSDK
 ) else (
 rem === BUILDING JavascriptSDK with params %* ===
-node generate.js ..\API_Specs javascript=..\sdks\JavascriptSDK\PlayFabSDK %*
+node generate.js ..\API_Specs javascript=..\sdks\JavaScriptSDK\PlayFabSDK %*
 )
 popd

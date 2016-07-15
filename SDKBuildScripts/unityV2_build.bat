085dcc05956610c2c3c8598774b032b6e5e6f8c4
pgilmorepf <paul@playfab.com>
2016-07-15 13:30:03 -0700

    Unity V2 is the normal for building UnitySdk now.

--- SDKBuildScripts/unityV2_build.bat
+++ SDKBuildScripts/unityV2_build.bat
@@ -1,9 +1,9 @@
 pushd ..
 if [%1] == [] (
 rem === BUILDING UnitySDK ===
-node generate.js ..\API_Specs csharp-unityv2=..\sdks\UnitySDKV2Beta
+node generate.js ..\API_Specs csharp-unityv2=..\sdks\UnitySDK -buildIdentifier test_manual_build
 ) else (
 rem === BUILDING UnitySDK with params %* ===
-node generate.js ..\API_Specs csharp-unityv2=..\sdks\UnitySDKV2Beta %*
+node generate.js ..\API_Specs csharp-unityv2=..\sdks\UnitySDK %*
 )
 popd
--- SDKBuildScripts/unity_build.bat
+++
@@ -1,9 +0,0 @@
-pushd ..
-if [%1] == [] (
-rem === BUILDING UnitySDK ===
-node generate.js ..\API_Specs csharp-unity=..\sdks\UnitySDK
-) else (
-rem === BUILDING UnitySDK with params %* ===
-node generate.js ..\API_Specs csharp-unity=..\sdks\UnitySDK %*
-)
-popd

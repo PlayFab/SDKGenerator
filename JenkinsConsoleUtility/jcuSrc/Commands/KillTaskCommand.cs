87594f9108b768e25e2593bf8da4adb253c8fb8c
pgilmorepf <paul@playfab.com>
2016-08-12 15:41:47 -0700

    Revising the task kill process in JCU
    Summary: Revising the task kill process in JCU
    
    Test Plan: Jenker!
    
    Reviewers: #devtools_in-progress, Marco.Williams
    
    Reviewed By: #devtools_in-progress, Marco.Williams
    
    Maniphest Tasks: T1856
    
    Differential Revision: https://phab.playfabdev.com/D2213

--- JenkinsConsoleUtility/jcuSrc/Commands/KillTaskCommand.cs
+++ JenkinsConsoleUtility/jcuSrc/Commands/KillTaskCommand.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 
 namespace JenkinsConsoleUtility.Commands
 {
     class KillTaskCommand : ICommand
     {
+        private const int TASK_KILL_DELAY_MS = 15000;
+        private const int TASK_KILL_SLEEP_MS = 500;
+
         public string commandKey { get { return "kill"; } }
 
         public int Execute(Dictionary<string, string> inputs)
@@ -14,14 +18,39 @@ namespace JenkinsConsoleUtility.Commands
             if (!inputs.TryGetValue("taskname", out taskName))
                 return 1;
 
-            var processes = Process.GetProcessesByName(taskName);
-            foreach (var process in processes)
+            List<Process> hitList = new List<Process>();
+            hitList.AddRange(Process.GetProcessesByName(taskName));
+            foreach (var eachProcess in hitList)
             {
-                JenkinsConsoleUtility.FancyWriteToConsole("Killing task: " + process.ProcessName, null, ConsoleColor.Magenta);
-                process.CloseMainWindow(); // Gently close the target so they don't throw error codes 
+                JenkinsConsoleUtility.FancyWriteToConsole("Closing task: " + eachProcess.ProcessName, null, ConsoleColor.Yellow);
+                eachProcess.CloseMainWindow(); // Gently close the target so they don't throw error codes 
             }
 
-            return processes.Length > 0 ? 0 : 1;
+            // Sleep for a while and wait for the programs to close safely
+            int openCount = hitList.Count;
+            for (int i = 0; i < TASK_KILL_DELAY_MS; i += TASK_KILL_SLEEP_MS)
+            {
+                Thread.Sleep(TASK_KILL_SLEEP_MS);
+                openCount = 0;
+                foreach (var eachProcess in hitList)
+                {
+                    if (!eachProcess.HasExited)
+                        openCount += 1;
+                }
+                if (openCount == 0)
+                    break;
+            }
+
+            // Time's up, close everything
+            foreach (var eachProcess in hitList)
+            {
+                if (eachProcess.HasExited)
+                    continue; // Finished skip it
+                JenkinsConsoleUtility.FancyWriteToConsole("Killing task: " + eachProcess.ProcessName, null, ConsoleColor.Red);
+                eachProcess.Kill(); // If it didn't close gently, then close it better.
+            }
+
+            return hitList.Count > 0 ? 0 : 1;
         }
     }
 }

18c3e6a6c18ba70824901e620f09ff540906dffa
pgilmorepf <paul@playfab.com>
2016-09-30 09:55:19 -0700

    Deleting V0 target
    Summary:
    Deleting V0 target
    fixes T2258
    
    Test Plan: None!  BUAHAHAHAHA
    
    Reviewers: Marco.Williams, #devtools_in-progress
    
    Reviewed By: Marco.Williams, #devtools_in-progress
    
    Maniphest Tasks: T2258
    
    Differential Revision: https://phab.playfabdev.com/D2543

--- SDKBuildScripts/unity_v0_build.bat
+++
@@ -1,9 +0,0 @@
-pushd ..
-if [%1] == [] (
-rem === BUILDING UnitySDK ===
-node generate.js ..\API_Specs unity-v0=..\sdks\UnitySDK -buildIdentifier test_manual_build
-) else (
-rem === BUILDING UnitySDK with params %* ===
-node generate.js ..\API_Specs unity-v0=..\sdks\UnitySDK %*
-)
-popd

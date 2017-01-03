65a7cf81c96aee8dc86bd254661f4c4300443456
pgilmorepf <paul@playfab.com>
2017-01-03 11:21:20 -0800

    Updating the arc-diff testing script to sync pf-main
    Summary: Updating the arc-diff testing script to sync pf-main
    
    Test Plan: Jenker!
    
    Reviewers: Marco.Williams
    
    Maniphest Tasks: T2610
    
    Differential Revision: https://phab.playfabdev.com/D2983

--- JenkinsConsoleUtility/JenkinsScripts/testInit.bat
+++ JenkinsConsoleUtility/JenkinsScripts/testInit.bat
@@ -1,51 +1,40 @@
-@echo off
+rem @echo off
 setlocal
-if [%1]==[] (
-    set gitTarget=%AUTOMATED_GIT_REPO%
-) ELSE (
-    set gitTarget=%1
-)
-
 cd %WORKSPACE%
 
-pushd SDKGenerator
-if %errorLevel% EQU 1 (
-    git clone git@github.com:PlayFab/SDKGenerator.git
-    pushd SDKGenerator
-)
-git reset head .
-git checkout -- .
-git clean -df
-git checkout master
-git pull origin master
-call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
-popd
-
-pushd API_Specs
-if %errorLevel% EQU 1 (
-    git clone git@github.com:PlayFab/API_Specs.git
-    pushd API_Specs
-)
-git reset head .
-git checkout -- .
-git clean -df
-git checkout master
-git pull origin master
-popd
-
-pushd sdks\%SdkName%
-if %errorLevel% EQU 1 (
-    mkdir sdks
-    pushd sdks
-    git clone git@github.com:PlayFab/%SdkName%.git
-    popd
-    pushd sdks\%SdkName%
-)
-git reset head .
-git checkout -- .
-git clean -df
-git checkout master
-git pull origin master
+call :syncRepo pf-main %PatchRepoName%
+call :syncRepo SDKGenerator %PatchRepoName%
+call :syncRepo API_Specs %PatchRepoName%
+call :forcePushD sdks
+call :syncRepo %SdkName% %PatchRepoName%
 popd
 
 endlocal
+exit %errorLevel%
+
+rem USAGE: call :forcePushD <path>
+:forcePushD
+pushd %1
+if %errorLevel% EQU 1 (
+    mkdir %1
+    pushd %1
+)
+goto :EOF
+
+rem USAGE: call :syncRepo <RepoName> <PatchRepoName>
+:syncRepo
+pushd %1
+if %errorLevel% EQU 1 (
+    git clone git@github.com:PlayFab/%1.git
+    pushd %1
+)
+git reset head .
+git checkout -- .
+git clean -df
+git checkout master
+git pull origin master
+if [%1]==[%2] (
+    call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
+)
+popd
+goto :EOF

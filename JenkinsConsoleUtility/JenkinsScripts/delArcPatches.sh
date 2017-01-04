48a8c3a86586f7b6c13e1e42a2ebdcb353dc39cd
pgilmorepf <paul@playfab.com>
2017-01-04 14:52:54 -0800

    Revisions to the Unity ArcDiff setup
    Summary:
    Revisions to the Unity ArcDiff setup
    Updating pf-main to use a shared directory for pf-main, to avoid excess copying
    Updating the bash script so it handles folders with spaces
    Some simple cleanup and logging
    
    Test Plan: Jekner!
    
    Reviewers: Marco.Williams
    
    Maniphest Tasks: T2610
    
    Differential Revision: https://phab.playfabdev.com/D2991

--- JenkinsConsoleUtility/JenkinsScripts/delArcPatches.sh
+++ JenkinsConsoleUtility/JenkinsScripts/delArcPatches.sh
@@ -1,2 +1,4 @@
-cd $1
+#!/bin/bash
+cd "$1"
+echo Deleting Arc-Patches in: ${PWD}
 git for-each-ref --format='%(refname:short)' refs/heads/arcpatch* | while read branch; do    BRANCH_EXISTS=$( git ls-remote --heads origin $branch | wc -l );    if [ $BRANCH_EXISTS -eq 0 ]; then        git branch -D $branch;    fi;done
--- JenkinsConsoleUtility/JenkinsScripts/testInit.bat
+++ JenkinsConsoleUtility/JenkinsScripts/testInit.bat
@@ -1,11 +1,16 @@
 @echo off
 setlocal
-cd %WORKSPACE%
 
+if [%SHARED_WORKSPACE%]==[] (
+    set SHARED_WORKSPACE=%WORKSPACE%\..\shared\%EXECUTOR_NUMBER%
+)
+
+call :forceCD %SHARED_WORKSPACE%
 call :syncRepo pf-main
 pushd pf-main\Server
 nuget restore Server.sln
 popd
+call :forceCD %WORKSPACE%
 call :syncRepo SDKGenerator
 call :syncRepo API_Specs
 call :forcePushD sdks
@@ -28,8 +33,18 @@ if %errorLevel% EQU 1 (
 )
 goto :EOF
 
-rem USAGE: call :syncRepo <RepoName> <PatchRepoName>
+rem USAGE: call :forceCD <path>
+:forceCD
+cd %1
+if %errorLevel% EQU 1 (
+    mkdir %1
+    cd %1
+)
+goto :EOF
+
+rem USAGE: call :syncRepo <RepoName>
 :syncRepo
+echo ==== syncRepo %cd% %1 ====
 pushd %1
 if %errorLevel% EQU 1 (
     git clone git@github.com:PlayFab/%1.git
@@ -42,28 +57,33 @@ git checkout master
 git pull origin master
 )
 popd
+echo ==== syncRepo Done ====
 goto :EOF
 
 rem USAGE: call :cleanArcPatches
 :cleanArcPatches
+echo ==== cleanArcPatches %cd% ====
 pushd SDKGenerator\JenkinsConsoleUtility\JenkinsScripts
-call delArcPatches.sh "%WORKSPACE%\pf-main"
+call delArcPatches.sh "%SHARED_WORKSPACE%\pf-main"
 call delArcPatches.sh "%WORKSPACE%\SDKGenerator"
 call delArcPatches.sh "%WORKSPACE%\sdks\%SdkName%"
+echo ==== cleanArcPatches Done ====
 goto :EOF
 
 rem USAGE: call :applyArcPatch
 :applyArcPatch
+echo ==== applyArcPatch %cd% %PatchRepoName% ====
 if [%PatchRepoName%]==[pf-main] (
-    cd %PatchRepoName%
+    cd %SHARED_WORKSPACE%\%PatchRepoName%
     call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
 )
 if [%PatchRepoName%]==[SDKGenerator] (
-    cd %PatchRepoName%
+    cd %WORKSPACE%\%PatchRepoName%
     call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
 )
 if [%PatchRepoName%]==[%SdkName%] (
-    cd sdks\%PatchRepoName%
+    cd %WORKSPACE%\sdks\%PatchRepoName%
     call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
 )
+echo ==== applyArcPatch Done ====
 goto :EOF

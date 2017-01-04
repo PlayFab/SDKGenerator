de9e3f1b431fb5f02eede531ab118c4a8de1fc91
pgilmorepf <paul@playfab.com>
2017-01-04 13:20:13 -0800

    Updating the arc-diff script to clean old arcpatch branches
    Summary: Updating the arc-diff script to clean old arcpatch branches
    
    Test Plan: Jenker!
    
    Reviewers: Marco.Williams
    
    Maniphest Tasks: T2610
    
    Differential Revision: https://phab.playfabdev.com/D2989

--- JenkinsConsoleUtility/JenkinsScripts/testInit.bat
+++ JenkinsConsoleUtility/JenkinsScripts/testInit.bat
@@ -2,18 +2,22 @@
 setlocal
 cd %WORKSPACE%
 
-call :syncRepo pf-main %PatchRepoName%
+call :syncRepo pf-main
 pushd pf-main\Server
 nuget restore Server.sln
 popd
-call :syncRepo SDKGenerator %PatchRepoName%
-call :syncRepo API_Specs %PatchRepoName%
+call :syncRepo SDKGenerator
+call :syncRepo API_Specs
 call :forcePushD sdks
-call :syncRepo %SdkName% %PatchRepoName%
+call :syncRepo %SdkName%
 popd
+
+call :cleanArcPatches
+call :applyArcPatch
 
 endlocal
 exit %errorLevel%
+
 
 rem USAGE: call :forcePushD <path>
 :forcePushD
@@ -36,8 +40,30 @@ git checkout -- .
 git clean -df
 git checkout master
 git pull origin master
-if [%1]==[%2] (
-    call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
 )
 popd
+goto :EOF
+
+rem USAGE: call :cleanArcPatches
+:cleanArcPatches
+pushd SDKGenerator\JenkinsConsoleUtility\JenkinsScripts
+call delArcPatches.sh "%WORKSPACE%\pf-main"
+call delArcPatches.sh "%WORKSPACE%\SDKGenerator"
+call delArcPatches.sh "%WORKSPACE%\sdks\%SdkName%"
+goto :EOF
+
+rem USAGE: call :applyArcPatch
+:applyArcPatch
+if [%PatchRepoName%]==[pf-main] (
+    cd %PatchRepoName%
+    call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
+)
+if [%PatchRepoName%]==[SDKGenerator] (
+    cd %PatchRepoName%
+    call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
+)
+if [%PatchRepoName%]==[%SdkName%] (
+    cd sdks\%PatchRepoName%
+    call arc patch %DIFF_NUMBER% --conduit-token %JENKINS_PHAB_TOKEN%
+)
 goto :EOF

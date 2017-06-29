a460e720896cd0f45af885a2fefd9b95a8fa3a26
pgilmorepf <paul@playfab.com>
2017-06-29 11:50:29 -0700

    Update Cocos to 3.15.1 without breaking 3.13.1
    Summary:
    Example build only works with vs 2017 now.
    fixes T3111
    
    Test Plan: Jenker!
    
    Reviewers: Marco.Williams
    
    Maniphest Tasks: T3111
    
    Differential Revision: https://phab.playfabdev.com/D4112

--- targets/cpp-cocos2dx/ExampleSource/Classes/PlayFabApiTest.cpp
+++ targets/cpp-cocos2dx/ExampleSource/Classes/PlayFabApiTest.cpp
@@ -1,8 +1,13 @@
 #define COCOS2D_DEBUG 1
 
+#include "cocos2d.h"
+
 #pragma comment(lib, "wldap32.lib")
 #pragma comment(lib, "ws2_32.lib")
+#if (COCOS2D_VERSION < 0x00031501) // These libs don't exist in cocos 3.15, but do exist in cocos 3.13 (3.14 is unknown)
 #pragma comment(lib, "libcurl_imp.lib")
-#pragma comment(lib, "libeay32.lib")
-#pragma comment(lib, "ssleay32.lib")
 #pragma comment(lib, "libzlib.lib")
+#else
+#pragma comment(lib, "libcurl.lib")
+#pragma comment(lib, "zlib.lib")
+#endif

8dfc8ef7bcd1a0c7d5a4c767d582ae5010e7f607
Paul Gilmore <paul@playfab.com>
2017-08-24 16:59:16 -0700

    More fault tolerance

--- JenkinsConsoleUtility/JenkinsScripts/util.sh
+++ JenkinsConsoleUtility/JenkinsScripts/util.sh
@@ -74,10 +74,10 @@ CleanCurrentRepo () {
     git fetch --progress origin || CleanCurrentRepo hard 3 $retryCounter
     if [ -n "$1" ]; then
         git reset --hard || CleanCurrentRepo hard 3 $retryCounter
-        git checkout master || CleanCurrentRepo hard 3 $retryCounter
+        git checkout master || git checkout -b master || CleanCurrentRepo hard 3 $retryCounter
         git reset --hard origin/master || CleanCurrentRepo hard 3 $retryCounter
     else
-        git checkout master || CleanCurrentRepo hard 3 $retryCounter
+        git checkout master || git checkout -b master || CleanCurrentRepo hard 3 $retryCounter
         git pull --ff-only || CleanCurrentRepo hard 3 $retryCounter
     fi
     git remote prune origin

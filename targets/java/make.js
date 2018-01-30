315eb07356bae94090ed741e27618ba3ca8ac167
Paul Gilmore <paul@playfab.com>
2018-01-30 13:16:48 -0800

    Minor Resharper fixes for JavaSDK generator script.

--- targets/java/make.js
+++ targets/java/make.js
@@ -8,139 +8,135 @@ if (typeof (getCompiledTemplate) === "undefined") getCompiledTemplate = function
 exports.makeClientAPI2 = function (apis, sourceDir, apiOutputDir) {
     var srcOutputLoc = ["", "../AndroidStudioExample/app/"];
     var testOutputLoc = ["", "../AndroidStudioExample/app/"];
-    
+
     for (var i = 0; i < srcOutputLoc.length; i++) {
         var srcOutputDir = path.resolve(apiOutputDir, srcOutputLoc[i]);
         var testOutputDir = path.resolve(apiOutputDir, testOutputLoc[i]);
         var isAndroid = srcOutputDir.indexOf("AndroidStudioExample") >= 0;
-        
+
         console.log("Generating Java client SDK to " + srcOutputDir);
         copyTree(path.resolve(sourceDir, "srcCode"), srcOutputDir);
         copyTree(path.resolve(sourceDir, "testingFiles/client"), testOutputDir);
-        MakeDatatypes(apis, sourceDir, srcOutputDir);
+        makeDatatypes(apis, sourceDir, srcOutputDir);
         for (var j = 0; j < apis.length; j++)
             makeApi(apis[j], sourceDir, apiOutputDir, false);
-        GenerateSimpleFiles(apis, "Client", sourceDir, srcOutputDir, isAndroid);
+        generateSimpleFiles(apis, "Client", sourceDir, srcOutputDir, isAndroid);
     }
 }
 
 exports.makeServerAPI = function (apis, sourceDir, apiOutputDir) {
     console.log("Generating Java server SDK to " + apiOutputDir);
-    
+
     copyTree(path.resolve(sourceDir, "srcCode"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "testingFiles/server"), apiOutputDir);
-    MakeDatatypes(apis, sourceDir, apiOutputDir);
+    makeDatatypes(apis, sourceDir, apiOutputDir);
     for (var i = 0; i < apis.length; i++)
         makeApi(apis[i], sourceDir, apiOutputDir, false);
-    GenerateSimpleFiles(apis, "Server", sourceDir, apiOutputDir);
+    generateSimpleFiles(apis, "Server", sourceDir, apiOutputDir);
 }
 
 exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
     console.log("Generating Java combined SDK to " + apiOutputDir);
-    
+
     copyTree(path.resolve(sourceDir, "srcCode"), apiOutputDir);
     copyTree(path.resolve(sourceDir, "testingFiles/combo"), apiOutputDir);
-    MakeDatatypes(apis, sourceDir, apiOutputDir);
+    makeDatatypes(apis, sourceDir, apiOutputDir);
     for (var i = 0; i < apis.length; i++)
         makeApi(apis[i], sourceDir, apiOutputDir, false);
-    GenerateSimpleFiles(apis, "Combo", sourceDir, apiOutputDir);
+    generateSimpleFiles(apis, "Combo", sourceDir, apiOutputDir);
 }
 
-function MakeDatatypes(apis, sourceDir, apiOutputDir) {
+function makeDatatypes(apis, sourceDir, apiOutputDir) {
     var templateDir = path.resolve(sourceDir, "templates");
     var modelTemplate = getCompiledTemplate(path.resolve(templateDir, "Model.java.ejs"));
     var modelsTemplate = getCompiledTemplate(path.resolve(templateDir, "Models.java.ejs"));
     var enumTemplate = getCompiledTemplate(path.resolve(templateDir, "Enum.java.ejs"));
-    
+
     var makeDatatype = function (datatype, api) {
-        var modelLocals = {};
-        modelLocals.datatype = datatype;
-        modelLocals.getPropertyDef = GetModelPropertyDef;
-        modelLocals.GetPropertyAttribs = GetPropertyAttribs;
-        modelLocals.generateApiSummary = generateApiSummary;
-        modelLocals.api = api;
-        return datatype.isenum ? enumTemplate(modelLocals) : modelTemplate(modelLocals);
+        var locals = {
+            api: api,
+            datatype: datatype,
+            getPropertyDef: getModelPropertyDef,
+            getPropertyAttribs: getPropertyAttribs,
+            generateApiSummary: generateApiSummary
+        };
+        return datatype.isenum ? enumTemplate(locals) : modelTemplate(locals);
     };
-    
+
     for (var a = 0; a < apis.length; a++) {
-        var modelsLocal = {
+        var locals = {
             api: apis[a],
             makeDatatype: makeDatatype
         };
-        writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFab" + apis[a].name + "Models.java"), modelsTemplate(modelsLocal));
+        writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFab" + apis[a].name + "Models.java"), modelsTemplate(locals));
     }
 }
 
 function makeApi(api, sourceDir, apiOutputDir, isAndroid) {
     console.log("Generating Java " + api.name + " library to " + apiOutputDir);
-    
-    var apiLocals = {
+
+    var locals = {
         api: api,
         isAndroid: isAndroid,
-        GetAuthParams: GetAuthParams,
-        GetRequestActions: GetRequestActions,
-        GetResultActions: GetResultActions,
-        GetUrlAccessor: GetUrlAccessor,
+        getAuthParams: getAuthParams,
+        getRequestActions: getRequestActions,
+        getResultActions: getResultActions,
+        getUrlAccessor: getUrlAccessor,
         generateApiSummary: generateApiSummary,
         hasClientOptions: api.name === "Client"
     };
-    
+
     var apiTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/API.java.ejs"));
-    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFab" + api.name + "API.java"), apiTemplate(apiLocals));
+    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFab" + api.name + "API.java"), apiTemplate(locals));
 }
 
-function GenerateSimpleFiles(apis, apiName, sourceDir, apiOutputDir, isAndroid) {
-    var errorsTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/Errors.java.ejs"));
-    var settingsTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/PlayFabSettings.java.ejs"));
-    var pomTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/pom.xml.ejs"));
-    
-    var errorLocals = {
-        errorList: apis[0].errorList,
-        errors: apis[0].errors
-    };
-    
-    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFabErrors.java"), errorsTemplate(errorLocals));
-    
-    var settingsLocals = {
-        sdkVersion: exports.sdkVersion,
+function generateSimpleFiles(apis, apiName, sourceDir, apiOutputDir, isAndroid) {
+
+    var locals = {
+        apiName: apiName,
+        apiNameLc: apiName.toLowerCase(),
         buildIdentifier: exports.buildIdentifier,
-        isAndroid: isAndroid,
+        errorList: apis[0].errorList,
+        errors: apis[0].errors,
         hasClientOptions: false,
-        hasServerOptions: false
+        hasServerOptions: false,
+        isAndroid: isAndroid,
+        sdkVersion: exports.sdkVersion,
     };
     for (var i = 0; i < apis.length; i++) {
         if (apis[i].name === "Client")
-            settingsLocals.hasClientOptions = true;
+            locals.hasClientOptions = true;
         else
-            settingsLocals.hasServerOptions = true;
+            locals.hasServerOptions = true;
     }
-    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFabSettings.java"), settingsTemplate(settingsLocals));
-    
+
+    var errorsTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/Errors.java.ejs"));
+    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFabErrors.java"), errorsTemplate(locals));
+
+    var settingsTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/PlayFabSettings.java.ejs"));
+    writeFile(path.resolve(apiOutputDir, "src/main/java/com/playfab/PlayFabSettings.java"), settingsTemplate(locals));
+
     if (!isAndroid) {
-        var pomLocals = {
-            apiName: apiName,
-            apiNameLc: apiName.toLowerCase(),
-            sdkVersion: exports.sdkVersion
-        };
-        writeFile(path.resolve(apiOutputDir, "pom.xml"), pomTemplate(pomLocals));
+        var pomTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/pom.xml.ejs"));
+        writeFile(path.resolve(apiOutputDir, "pom.xml"), pomTemplate(locals));
     }
 }
 
-function GetModelPropertyDef(property, datatype) {
-    var basicType = GetPropertyJavaType(property, datatype);
+function getModelPropertyDef(property, datatype) {
+    var basicType = getPropertyJavaType(property, datatype);
     if (property.collection && property.collection === "array")
         return "ArrayList<" + basicType + "> " + property.name;
     else if (property.collection && property.collection === "map")
         return "Map<String," + basicType + "> " + property.name;
     else if (property.collection)
         throw "Unknown collection type: " + property.collection + " for " + property.name + " in " + datatype.name;
-    
-    return GetPropertyJavaType(property, datatype) + " " + property.name;
+
+    return getPropertyJavaType(property, datatype) + " " + property.name;
 }
 
-function GetPropertyAttribs(tabbing, property, datatype, api) {
+function getPropertyAttribs(tabbing, property, datatype, api) {
     var attribs = "";
-    
+
     if (property.isUnordered) {
         var listDatatype = api.datatypes[property.actualtype];
         if (listDatatype && listDatatype.sortKey)
@@ -148,13 +144,13 @@ function GetPropertyAttribs(tabbing, property, datatype, api) {
         else
             attribs += tabbing + "@Unordered\n";
     }
-    
+
     return attribs;
 }
 
-function GetPropertyJavaType(property, datatype) {
+function getPropertyJavaType(property, datatype) {
     var optional = "";
-    
+
     if (property.actualtype === "String")
         return "String";
     else if (property.actualtype === "Boolean")
@@ -186,7 +182,7 @@ function GetPropertyJavaType(property, datatype) {
     throw "Unknown property type: " + property.actualtype + " for " + property.name + " in " + datatype.name;
 }
 
-function GetAuthParams(apiCall) {
+function getAuthParams(apiCall) {
     if (apiCall.auth === "SecretKey")
         return "\"X-SecretKey\", PlayFabSettings.DeveloperSecretKey";
     else if (apiCall.auth === "SessionTicket")
@@ -194,27 +190,28 @@ function GetAuthParams(apiCall) {
     return "null, null";
 }
 
-function GetRequestActions(apiCall, api) {
-    if (api.name === "Client" && (apiCall.result === "LoginResult" || apiCall.request === "RegisterPlayFabUserRequest"))
-        return "        request.TitleId = PlayFabSettings.TitleId != null ? PlayFabSettings.TitleId : request.TitleId;\n        if(request.TitleId == null) throw new Exception (\"Must be have PlayFabSettings.TitleId set to call this method\");\n";
-    if (api.name === "Client" && apiCall.auth === "SessionTicket")
-        return "        if (_authKey == null) throw new Exception (\"Must be logged in to call this method\");\n";
+function getRequestActions(tabbing, apiCall) {
+    if (apiCall.result === "LoginResult" || apiCall.request === "RegisterPlayFabUserRequest")
+        return tabbing + "request.TitleId = PlayFabSettings.TitleId != null ? PlayFabSettings.TitleId : request.TitleId;\n"
+            + tabbing + "if(request.TitleId == null) throw new Exception (\"Must be have PlayFabSettings.TitleId set to call this method\");\n";
+    if (apiCall.auth === "SessionTicket")
+        return tabbing + "if (_authKey == null) throw new Exception (\"Must be logged in to call this method\");\n";
     if (apiCall.auth === "SecretKey")
-        return "        if (PlayFabSettings.DeveloperSecretKey == null) throw new Exception (\"Must have PlayFabSettings.DeveloperSecretKey set to call this method\");\n";
+        return tabbing + "if (PlayFabSettings.DeveloperSecretKey == null) throw new Exception (\"Must have PlayFabSettings.DeveloperSecretKey set to call this method\");\n";
     return "";
 }
 
-function GetResultActions(apiCall, api) {
+function getResultActions(tabbing, apiCall, api) {
     if (api.name === "Client" && (apiCall.result === "LoginResult" || apiCall.result === "RegisterPlayFabUserResult"))
-        return "        _authKey = result.SessionTicket != null ? result.SessionTicket : _authKey;\n" 
-            + "        MultiStepClientLogin(resultData.data.SettingsForUser.NeedsAttribution);\n";
+        return tabbing + "_authKey = result.SessionTicket != null ? result.SessionTicket : _authKey;\n"
+            + tabbing + "MultiStepClientLogin(resultData.data.SettingsForUser.NeedsAttribution);\n";
     else if (api.name === "Client" && apiCall.result === "AttributeInstallResult")
-        return "        // Modify AdvertisingIdType:  Prevents us from sending the id multiple times, and allows automated tests to determine id was sent successfully\n" 
-            + "        PlayFabSettings.AdvertisingIdType += \"_Successful\";\n";
+        return tabbing + "// Modify AdvertisingIdType:  Prevents us from sending the id multiple times, and allows automated tests to determine id was sent successfully\n"
+            + tabbing + "PlayFabSettings.AdvertisingIdType += \"_Successful\";\n";
     return "";
 }
 
-function GetUrlAccessor(apiCall) {
+function getUrlAccessor() {
     return "PlayFabSettings.GetURL()";
 }
 

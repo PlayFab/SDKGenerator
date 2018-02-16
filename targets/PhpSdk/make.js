b94b05c34f0d97dc6ffef3a0344ddc7382c3ea07
Paul Gilmore <paul@playfab.com>
2018-02-15 17:21:16 -0800

    PHP Entity API
    Summary: PHP Entity API
    
    Test Plan: almost nothing
    
    Reviewers: paul
    
    Reviewed By: paul
    
    Maniphest Tasks: T3496
    
    Differential Revision: https://phabricator.playfab.com/D5390

--- targets/PhpSdk/make.js
+++ targets/PhpSdk/make.js
@@ -22,34 +22,43 @@ function makeApi(api, sourceDir, apiOutputDir) {
         api: api,
         sdkVersion: exports.sdkVersion,
         generateApiSummary: generateApiSummary,
-        getAuthKey: getAuthKey,
-        getRequestActions: getRequestActions
+        getAuthInputParams: getAuthInputParams,
+        getCurlAuthParams: getCurlAuthParams,
+        getCustomApiSignatures: getCustomApiSignatures,
+        getRequestActions: getRequestActions,
+        sourceDir: sourceDir
     };
 
     var apiTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/PlayFabApi.php.ejs"));
     writeFile(path.resolve(apiOutputDir, "PlayFab" + api.name + "Api.php"), apiTemplate(locals));
 }
 
-function getAuthKey(apiCall) {
+function getCurlAuthParams(apiCall) {
+    if (apiCall.url === "/Authentication/GetEntityToken")
+        return "$authKey, $authValue";
+    if (apiCall.auth === "EntityToken")
+        return "\"X-EntityToken\", $entityToken";
+    if (apiCall.auth === "SessionTicket")
+        return "\"X-Authentication\", $clientSessionTicket";
     if (apiCall.auth === "SecretKey")
-        return "\"X-SecretKey\"";
-    else if (apiCall.auth === "SessionTicket")
-        return "\"X-Authentication\"";
-    else if (apiCall.auth === "EntityToken")
-        return "\"X-EntityToken\"";
-    return "null";
+        return "\"X-SecretKey\", $developerSecreteKey";
+    return "null, null";
 }
 
 function getRequestActions(tabbing, apiCall) {
+    if (apiCall.url === "/Authentication/GetEntityToken")
+        return tabbing + "if (!is_null($entityToken)) { $authKey = \"X-EntityToken\"; $authValue = $entityToken; }\n"
+            + tabbing + "elseif (!is_null($clientSessionTicket)) { $authKey = \"X-Authentication\"; $authValue = $clientSessionTicket; }\n"
+            + tabbing + "elseif (!is_null($developerSecreteKey)) { $authKey = \"X-SecretKey\"; $authValue = $developerSecreteKey; }\n";
     if (apiCall.result === "LoginResult" || apiCall.request === "RegisterPlayFabUserRequest")
         return tabbing + "if (!isset($titleId)) $titleId = PlayFabSettings::$titleId;\n"
             + tabbing + "if (!isset($request->$titleId)) !$request->titleId = $titleId;\n";
+    if (apiCall.auth === "EntityToken")
+        return tabbing + "//TODO: Check the entityToken\n";
     if (apiCall.auth === "SessionTicket")
         return tabbing + "//TODO: Check the sessionTicket\n";
     if (apiCall.auth === "SecretKey")
         return tabbing + "//TODO: Check the devSecretKey\n";
-    if (apiCall.auth === "EntityToken")
-        return tabbing + "//TODO: Check the entityToken\n";
 
     return "";
 }
@@ -65,4 +74,32 @@ function generateApiSummary(tabbing, apiElement, summaryParam, extraLines) {
     else
         output = "";
     return output;
+}
+
+function getAuthInputParams(apiCall) {
+    if (apiCall.url === "/Authentication/GetEntityToken")
+        return "$entityToken, $clientSessionTicket, $developerSecreteKey, ";
+    if (apiCall.auth === "EntityToken")
+        return "$entityToken, ";
+    if (apiCall.auth === "SessionTicket")
+        return "$clientSessionTicket, ";
+    if (apiCall.auth === "SecretKey")
+        return "$developerSecreteKey, ";
+    return "";
+}
+
+function getCustomApiSignatures(api, sourceDir, apiCall) {
+    if (apiCall.url === "/Authentication/GetEntityToken") {
+        var locals = {
+            api: api,
+            apiCall: apiCall,
+            generateApiSummary: generateApiSummary,
+            getAuthInputParams: getAuthInputParams,
+            getCurlAuthParams: getCurlAuthParams,
+            getRequestActions: getRequestActions,
+        };
+        var customTemplate = getCompiledTemplate(path.resolve(sourceDir, "templates/GetEntityTokenExtra.php.ejs"));
+        return customTemplate(locals);
+    }
+    return "";
 }

fc255154feb954ade3fe4237c5ff6988ca9a8d0c
Paul Gilmore <pagilmor@microsoft.com>
2018-04-06 10:22:19 -0700

    Secret key cannot be restricted to a specific build anymore.

--- targets/unity-v2/source/Shared/Models/PlayFabSharedSettings.cs
+++ targets/unity-v2/source/Shared/Models/PlayFabSharedSettings.cs
@@ -7,9 +7,7 @@ using PlayFab;
 public class PlayFabSharedSettings : ScriptableObject
 {
     public string TitleId;
-#if ENABLE_PLAYFABADMIN_API || ENABLE_PLAYFABSERVER_API || UNITY_EDITOR
     public string DeveloperSecretKey;
-#endif
 #if ENABLE_PLAYFABPLAYSTREAM_API && ENABLE_PLAYFABSERVER_API
     public string ProductionEnvironmentPlayStreamUrl = "";
 #endif

291f8c53e64593720c59bb9723f174147c3a8cad
Paul Gilmore <pagilmor@microsoft.com>
2018-06-14 16:59:33 -0700

    Fixing an entity test that fails (#137)
    * Fixing an entity test that fails if it's the first time it's been run on this entity.
    
    * Stupid spacing problem.

--- targets/javascript/make.js
+++ targets/javascript/make.js
@@ -88,10 +88,10 @@ function getResultActions(tabbing, apiCall) {
         return tabbing + "if (result != null) {\n"
             + tabbing + "       if(result.data.SessionTicket != null) {\n"
             + tabbing + "           PlayFab._internalSettings.sessionTicket = result.data.SessionTicket;\n"
-            + tabbing + "       }"
+            + tabbing + "       }\n"
             + tabbing + "       if (result.data.EntityToken != null) {\n"
             + tabbing + "           PlayFab._internalSettings.entityToken = result.data.EntityToken.EntityToken;\n"
-            + tabbing + "       }"
+            + tabbing + "       }\n"
             + tabbing + "    PlayFab.ClientApi._MultiStepClientLogin(result.data.SettingsForUser.NeedsAttribution);\n"
             + tabbing + "}";
     if (apiCall.result === "RegisterPlayFabUserResult")

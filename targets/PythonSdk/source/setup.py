1b65e60c2dddafa45261782728b43e92d6765cbb
Todd Bello <toddbell@microsoft.com>
2018-06-26 11:50:13 -0700

    adding init and setup files to generation process.

---
+++ targets/PythonSdk/source/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+        
+setuptools.setup(
+    name="PlayFab",
+    version="0.0.1",
+    author="Todd Bello",
+    author_email="toddbell@microsoft.com",
+    description="PlayFab SDK for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PlayFab/PythonSdk",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

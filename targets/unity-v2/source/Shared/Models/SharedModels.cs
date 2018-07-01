d2b188ad42d0abc3a99161e2c11b72c1acef2a66
Ian Oliver <ian@wildcardstudios.com>
2018-07-01 13:34:45 -0700

    Adding Coroutine wrappers for API methods in Unity SDk

--- targets/unity-v2/source/Shared/Models/SharedModels.cs
+++ targets/unity-v2/source/Shared/Models/SharedModels.cs
@@ -16,4 +16,27 @@ namespace PlayFab.SharedModels
         public PlayFabRequestCommon Request;
         public object CustomData;
     }
+
+    public class WaitForPlayFabResponse<T> : UnityEngine.CustomYieldInstruction where T:PlayFabResultCommon
+    {
+        public T Result;
+        public PlayFabError Error;
+
+        bool complete = false;
+
+        public override bool keepWaiting { get { return !complete; } }
+        public bool IsError { get { return Error != null; } }
+
+        public void Complete(T r) {
+            Result = r;
+            Error = null;
+            complete = true;
+        }
+
+        public void Complete(PlayFabError e) {
+            Result = null;
+            Error = e;
+            complete = true;
+        }
+    }
 }

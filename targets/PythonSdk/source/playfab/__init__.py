8c6050bfc9990bfee9b858de27038193476d1777
Todd Bello <toddbell@microsoft.com>
2018-07-02 10:43:07 -0700

    adding the files to the correct, all lower case /playfab/ folder.

---
+++ targets/PythonSdk/source/playfab/__init__.py
@@ -0,0 +1,10 @@
+import playfab.PlayFabAdminAPI as PlayFabAdminAPI
+import playfab.PlayFabClientAPI as PlayFabClientAPI
+import playfab.PlayFabEntityAPI as PlayFabEntityAPI
+import playfab.PlayFabErrors as PlayFabErrors
+import playfab.PlayFabHTTP as PlayFabHTTP
+import playfab.PlayFabMatchmakerAPI as PlayFabMatchmakerAPI
+import playfab.PlayFabServerAPI as PlayFabServerAPI
+import playfab.PlayFabSettings as PlayFabSettings
+
+__all__ = ["PlayFabAdminAPI", "PlayFabClientAPI", "PlayFabEntityAPI", "PlayFabErrors", "PlayFabHTTP", "PlayFabMatchmakerAPI", "PlayFabServerAPI", "PlayFabSettings"]

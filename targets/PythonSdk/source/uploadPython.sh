494e2ff978b692ad7138b137a7b34a5c70845528
Todd Bello <toddbell@microsoft.com>
2018-07-02 17:23:37 -0700

    changing upload python script from powershell to bash now that the Jenkins job works as expected.

---
+++ targets/PythonSdk/source/uploadPython.sh
@@ -0,0 +1,13 @@
+echo ===== BEGAN SECOND SCRIPT =====
+
+cd sdks/$SdkName/
+
+python -m pip install --user --upgrade setuptools wheel
+
+python -m pip install --user --upgrade twine
+
+python setup.py sdist bdist_wheel
+
+twine upload dist/* -u playfabDevTools -p $PYTHON_API_HASH
+
+exit 0

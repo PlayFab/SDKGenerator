59a25c23b86f9e83fceb108c04c119a9e32565b1
Jordan Maxwell <nxtlabs13@gmail.com>
2018-07-16 12:02:40 -0500

    targets/PythonSdk: fixed issues #5 and #6 for the PythonSdk (#181)

--- targets/PythonSdk/make.js
+++ targets/PythonSdk/make.js
@@ -207,17 +207,20 @@ function getRequestActions(tabbing, apiCall) {
 
 function getResultActions(tabbing, apiCall, api) {
     if (apiCall.result === "LoginResult")
-        return tabbing + "PlayFabSettings._internalSettings.ClientSessionTicket = playFabResult[\"SessionTicket\"] or PlayFabSettings._internalSettings.ClientSessionTicket\n"
-            + tabbing + "PlayFabSettings._internalSettings.EntityToken = playFabResult[\"EntityToken\"][\"EntityToken\"] or PlayFabSettings._internalSettings.EntityToken\n"
-            + tabbing + "MultiStepClientLogin(playFabResult[\"SettingsForUser\"])\n";
+        return tabbing + "if playFabResult:\n" 
+            + tabbing + "    PlayFabSettings._internalSettings.ClientSessionTicket = playFabResult[\"SessionTicket\"] if \"SessionTicket\" in playFabResult else PlayFabSettings._internalSettings.ClientSessionTicket\n"
+            + tabbing + "    PlayFabSettings._internalSettings.EntityToken = playFabResult[\"EntityToken\"][\"EntityToken\"] if \"EntityToken\" in playFabResult else PlayFabSettings._internalSettings.EntityToken\n"
+            + tabbing + "    MultiStepClientLogin(playFabResult.get(\"SettingsForUser\"))\n";
     else if (apiCall.result === "RegisterPlayFabUserResult")
-        return tabbing + "PlayFabSettings._internalSettings.ClientSessionTicket = playFabResult[\"SessionTicket\"] or PlayFabSettings._internalSettings.ClientSessionTicket\n"
-            + tabbing + "MultiStepClientLogin(playFabResult[\"SettingsForUser\"])\n";
+        return tabbing + "if playFabResult:\n" 
+            + tabbing + "    PlayFabSettings._internalSettings.ClientSessionTicket = playFabResult[\"SessionTicket\"] if \"SessionTicket\" in playFabResult else PlayFabSettings._internalSettings.ClientSessionTicket\n"
+            + tabbing + "    MultiStepClientLogin(playFabResult.get(\"SettingsForUser\"))\n";
     else if (api.name === "Client" && apiCall.result === "AttributeInstallResult")
         return tabbing + "# Modify AdvertisingIdType:  Prevents us from sending the id multiple times, and allows automated tests to determine id was sent successfully\n"
             + tabbing + "PlayFabSettings.AdvertisingIdType += \"_Successful\"\n";
     else if (apiCall.result === "GetEntityTokenResponse")
-        return tabbing + "PlayFabSettings._internalSettings.EntityToken = playFabResult[\"EntityToken\"] or PlayFabSettings._internalSettings.EntityToken\n";
+        return tabbing + "if playFabResult:\n"
+            + tabbing + "    PlayFabSettings._internalSettings.EntityToken = playFabResult[\"EntityToken\"] if \"EntityToken\" in playFabResult else PlayFabSettings._internalSettings.EntityToken\n";
     return "";
 }
 

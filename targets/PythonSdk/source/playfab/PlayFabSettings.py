6f3fe38886dd7542b39f651e7d0e21d1d8b26139
Todd Bello <toddbell@microsoft.com>
2018-07-26 12:17:30 -0700

    removed excess # symbols.

--- targets/PythonSdk/source/playfab/PlayFabSettings.py
+++ targets/PythonSdk/source/playfab/PlayFabSettings.py
@@ -4,38 +4,38 @@ import traceback
 
 ProductionEnvironmentURL = "https://{titleId}.playfabapi.com{methodUrl}"
 """
-# You must set this value for PlayFabSdk to work properly (Found in the Game
-# Manager for your title, at the PlayFab Website)
+You must set this value for PlayFabSdk to work properly (Found in the Game
+Manager for your title, at the PlayFab Website)
 """
 
 TitleId = ""
 
 """
-# You must set this value for Admin/Server/Matchmaker to work properly (Found in the Game
-# Manager for your title, at the PlayFab Website)
+You must set this value for Admin/Server/Matchmaker to work properly (Found in the Game
+Manager for your title, at the PlayFab Website)
 """
 
 DeveloperSecretKey = None 
 
 """
-# Client specifics
+Client specifics
 """
 
 """
-# Set this to the appropriate AD_TYPE_X constant below
+Set this to the appropriate AD_TYPE_X constant below
 """
 AdvertisingIdType = "" 
 
 """
-# Set this to corresponding device value
+Set this to corresponding device value
 """
 AdvertisingIdValue = None 
 
 """
-# DisableAdvertising is provided for completeness, but changing it is not
-# suggested
-# Disabling this may prevent your advertising-related PlayFab marketplace
-# partners from working correctly
+DisableAdvertising is provided for completeness, but changing it is not
+suggested
+Disabling this may prevent your advertising-related PlayFab marketplace
+partners from working correctly
 """
 
 DisableAdvertising = False
@@ -48,12 +48,12 @@ class InternalSettings:
 _internalSettings = InternalSettings()
 
 """
-# This is automatically populated by the PlayFabEntityApi.GetEntityToken method.
+This is automatically populated by the PlayFabEntityApi.GetEntityToken method.
 """
 _internalSettings.EntityToken = None
 
 """
-# This is automatically populated by any PlayFabClientApi.Login method.
+This is automatically populated by any PlayFabClientApi.Login method.
 """
 _internalSettings.ClientSessionTicket = None
 _internalSettings.SdkVersionString = "PythonSdk-<%- sdkVersion %>"

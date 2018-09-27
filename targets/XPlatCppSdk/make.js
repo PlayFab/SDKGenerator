8816aa71a0a393213a3b85b14e2850521447af3b
Todd Bello <toddbell@microsoft.com>
2018-09-27 14:22:13 -0700

    adding verticalization to xplat (#299)
    * adding verticalization
    
    * addressing comments.
    
    * changing name, making length a function call.

--- targets/XPlatCppSdk/make.js
+++ targets/XPlatCppSdk/make.js
@@ -17,7 +17,8 @@ exports.makeCombinedAPI = function (apis, sourceDir, apiOutputDir) {
         sdkDate: exports.sdkVersion.split(".")[2],
         sdkYear: exports.sdkVersion.split(".")[2].substr(0, 2),
         vsVer: "v141", // As C++ versions change, we may need to update this
-        vsYear: "2017" // As VS versions change, we may need to update this
+        vsYear: "2017", // As VS versions change, we may need to update this
+        getVerticalNameDefault: getVerticalNameDefault
     };
 
     templatizeTree(locals, path.resolve(sourceDir, "source"), apiOutputDir);
@@ -268,4 +269,11 @@ function ifHasProps(datatype, displayText) {
     if (datatype.properties.length === 0)
         return "";
     return displayText;
+}
+
+function getVerticalNameDefault() {
+    if (exports.verticalName) {
+        return exports.verticalName;
+    }
+    return "";
 }

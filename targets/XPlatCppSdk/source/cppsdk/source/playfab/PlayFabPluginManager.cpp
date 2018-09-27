93734220700a99b2c1d00ee9ee98656035acab51
Vicodex <victorku@microsoft.com>
2018-09-27 09:21:34 -0700

    add a call to plugin manager to get transport plugin'

--- targets/XPlatCppSdk/source/cppsdk/source/playfab/PlayFabPluginManager.cpp
+++ targets/XPlatCppSdk/source/cppsdk/source/playfab/PlayFabPluginManager.cpp
@@ -5,7 +5,7 @@
 
 namespace PlayFab
 {
-    PlayFabPluginManager& PlayFabPluginManager::Instance()
+    PlayFabPluginManager& PlayFabPluginManager::GetInstance()
     {
         static PlayFabPluginManager instance;
         return instance;
@@ -13,7 +13,7 @@ namespace PlayFab
 
     void PlayFabPluginManager::SetPlugin(IPlayFabPlugin& plugin, const PlayFabPluginContract& contract, const std::string& instanceName)
     {
-        Instance().SetPluginInternal(plugin, contract, instanceName);
+        GetInstance().SetPluginInternal(plugin, contract, instanceName);
     }
 
     IPlayFabPlugin& PlayFabPluginManager::GetPluginInternal(const PlayFabPluginContract& contract, const std::string& instanceName)

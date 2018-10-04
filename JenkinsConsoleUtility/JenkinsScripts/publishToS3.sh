ae46797caa2f7b4b9a5f947cf929e2629d116a73
pgilmore <pagilmor@microsoft.com>
2018-10-04 13:17:35 -0700

    publishToS3 implementation
    TODO: S3BuildNum needs to be relayed through in Jenkins, but this script is right

--- JenkinsConsoleUtility/JenkinsScripts/publishToS3.sh
+++ JenkinsConsoleUtility/JenkinsScripts/publishToS3.sh
@@ -1,3 +1,20 @@
-echo "This file will contain the Publish To S3 logic which zips and writes a repo to S3."
-echo "It is currently a placeholder, so that Jenkins can start calling it, but doesn't actually do anything yet"
+#!/bin/bash
 
+. "$WORKSPACE/SDKGenerator/JenkinsConsoleUtility/JenkinsScripts/util.sh" 2> /dev/null || . ./util.sh 2> /dev/null
+. "$WORKSPACE/SDKGenerator/JenkinsConsoleUtility/JenkinsScripts/sdkUtil.sh" 2> /dev/null || . ./sdkUtil.sh 2> /dev/null
+
+DoWork() {
+    cd "$WORKSPACE"
+    pushd "sdks/$SdkName"
+    git clean -dfx
+    popd
+    
+    rm -f repo.zip
+    7z a -r repo.zip "sdks/$SdkName"
+
+    aws s3 cp repo.zip s3://playfab-sdk-dist/$VerticalName/$SdkName/$(date +%y%d%m)_${S3BuildNum}_$SdkName.zip
+}
+
+if [ "$PublishToS3" = "true" ]; then
+    DoWork
+fi

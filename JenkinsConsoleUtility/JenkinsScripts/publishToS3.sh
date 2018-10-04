3c578ab15bc1ef8c0102fb40625081c446fd41a6
pgilmore <pagilmor@microsoft.com>
2018-10-04 15:53:21 -0700

    Master is a valid vertical, but VerticalName is empty under that condition

--- JenkinsConsoleUtility/JenkinsScripts/publishToS3.sh
+++ JenkinsConsoleUtility/JenkinsScripts/publishToS3.sh
@@ -12,6 +12,7 @@ DoWork() {
     rm -f repo.zip
     7z a -r repo.zip "sdks/$SdkName"
 
+    CheckDefault VerticalName master
     aws s3 cp repo.zip s3://playfab-sdk-dist/$VerticalName/$SdkName/$(date +%y%d%m)_${S3BuildNum}_$SdkName.zip
 }
 

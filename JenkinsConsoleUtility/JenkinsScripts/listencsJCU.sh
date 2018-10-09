bbc87c721af894987598b926b79d7383f06613a3
pgilmore <pagilmor@microsoft.com>
2018-10-09 10:06:14 -0700

    Copy paste error in Jenkins led to a handful of failures
    Move this script into GitHub, and only do the kill-task option when it's required.

---
+++ JenkinsConsoleUtility/JenkinsScripts/listencsJCU.sh
@@ -0,0 +1,6 @@
+echo === Execute $SdkName UnitTests ===
+cd "$WORKSPACE\SDKGenerator\JenkinsConsoleUtility\bin\Debug"
+./JenkinsConsoleUtility.exe --listencs -buildIdentifier JBuild_${SdkName}_${VerticalName}_${NODE_NAME}_${EXECUTOR_NUMBER} -workspacePath "$WORKSPACE" -timeout 60 -verbose true
+if [ ! -z "$killTaskName" ]; then
+    ./JenkinsConsoleUtility.exe --kill -taskname $killTaskName
+fi

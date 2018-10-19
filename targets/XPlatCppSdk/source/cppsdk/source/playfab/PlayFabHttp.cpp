58a4ec083efe8d5edddb72eb3793088539adb76e
Vicodex <19293802+Vicodex@users.noreply.github.com>
2018-10-19 13:23:52 -0700

    added a better fix for JSON reader based on Scotts request (#321)

--- targets/XPlatCppSdk/source/cppsdk/source/playfab/PlayFabHttp.cpp
+++ targets/XPlatCppSdk/source/cppsdk/source/playfab/PlayFabHttp.cpp
@@ -188,10 +188,9 @@ namespace PlayFab
         else
         {
             Json::CharReaderBuilder jsonReaderFactory;
-            Json::CharReader* jsonReader(jsonReaderFactory.newCharReader());
+            std::unique_ptr<Json::CharReader> jsonReader(jsonReaderFactory.newCharReader());
             JSONCPP_STRING jsonParseErrors;
             const bool parsedSuccessfully = jsonReader->parse(reqContainer.responseString.c_str(), reqContainer.responseString.c_str() + reqContainer.responseString.length(), &reqContainer.responseJson, &jsonParseErrors);
-            delete jsonReader;
 
             if (parsedSuccessfully)
             {

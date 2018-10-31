3afca25f99fca1321584ba5bb4e8ba8bf1b86cb8
Todd Bello <toddbell@microsoft.com>
2018-10-31 13:13:01 -0700

    cleaning up some leftover test debugging issues Paul caught in review.

--- targets/unity-v2/Testing/Tests/PersistentSockets/PubSubTest.cs
+++ targets/unity-v2/Testing/Tests/PersistentSockets/PubSubTest.cs
@@ -11,14 +11,30 @@ namespace PlayFab.UUnit
     {
         static PlayFab.EventsModels.EntityKey _MyEntityKey = null;
         static string _previousTitleId = null;
+        public override void SetUp(UUnitTestContext testContext)
+        {
+            // specific title id for relay test
+            _previousTitleId = PlayFabSettings.TitleId;
+            PlayFabSettings.TitleId = "70B02F89";
+            PlayFabSettings.VerticalName = "spi-relay";
+        }
 
         public override void Tick(UUnitTestContext testContext)
         {
             // No async work needed?
             // should this be a theoretical HubConnection SendAsync call to print a message out in the test?
         }
+
         public override void TearDown(UUnitTestContext testContext)
         {
+            PlayFabSettings.TitleId = _previousTitleId;
+            PlayFabSettings.VerticalName = null;
+
+            // if test didn't complete, fail
+            if (testContext.ActiveState != UUnitActiveState.COMPLETE)
+            {
+                testContext.Fail("PubSub UnitTest didn't recieve a message in time");
+            }
         }
 
         public override void ClassTearDown()
@@ -28,44 +44,31 @@ namespace PlayFab.UUnit
         [UUnitTest]
         public void TestPubSubConstruction(UUnitTestContext testContext)
         {
-            //_testContext = testContext;
-            Debug.Log("Starting PubSub UnitTest");
-
-            // specific title id for relay test
-            _previousTitleId = PlayFabSettings.TitleId;
-            PlayFabSettings.TitleId = "70B02F89";
-            PlayFabSettings.VerticalName = "spi-relay";
-
             ClientModels.LoginWithCustomIDRequest login = new ClientModels.LoginWithCustomIDRequest();
-            login.CustomId = "ToddIsTryingToTestPersistentSocketsInUnity" + Guid.NewGuid().ToString();
+            login.CustomId = "PersistentSocketsUnityUnitTest" + Guid.NewGuid().ToString();
             login.CreateAccount = true;
             PlayFabClientAPI.LoginWithCustomID(login, LoginSuccess, LoginFailure, testContext);
         }
 
         void LoginSuccess(ClientModels.LoginResult result)
         {
-            // This needs to be set in PlayFabSettings?... worst case scenario we pass this through the constructor
             _MyEntityKey = new PlayFab.EventsModels.EntityKey { Id = result.EntityToken.Entity.Id, Type = result.EntityToken.Entity.Type };
 
-            // TODO: construct a PubSub object here
             PubSub pubSub = new PubSub(message =>
             {
-                Console.WriteLine(message.ToString());
                 Debug.Log("Received a Message: " + message);
-            }, new Topic { EventNamespace = "com.playfab.events.test", Name = "testevent", Entity = new Entity { Type = _MyEntityKey.Type, Id = _MyEntityKey.Id } }); // will this Entity conflict?
+                ((UUnitTestContext) result.CustomData).EndTest(UUnitFinishState.PASSED, "PubSub construction successful");
+            }, 
+            new Topic { EventNamespace = "com.playfab.events.test", Name = "testevent", Entity = new Entity { Type = _MyEntityKey.Type, Id = _MyEntityKey.Id } }); // will this Entity conflict?
 
             // see if you can print some messages out from here
             pubSub.PumpMessagesTest();
-
-            Debug.Log("Ending PubSub UnitTest");
-            ((UUnitTestContext) result.CustomData).EndTest(UUnitFinishState.PASSED, "PubSub construction successful****************************************************************************************************");
-            PlayFabSettings.TitleId = _previousTitleId;
-            PlayFabSettings.VerticalName = null;
         }
 
         void LoginFailure(PlayFab.PlayFabError error)
         {
-            Debug.Log("PubSub UnitTest Login Failed with the message: " + error.ErrorMessage);
+            // This needs to come from somewhere else?
+            ((UUnitTestContext) error.CustomData).Fail("PubSub UnitTest Login Failed with the message: " + error.ErrorMessage);
         }
     }
 }

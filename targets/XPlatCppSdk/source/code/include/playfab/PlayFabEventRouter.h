dd960e287b5307b34d07cf84c363efe8fb3083ea
Vicodex <19293802+Vicodex@users.noreply.github.com>
2018-12-12 16:36:24 -0800

    Implemented OneDS pipeline, minor fixes (#358)
    * implemented OneDS pipeline, minor fixes
    
    * addressed feedback
    
    * updated comment

--- targets/XPlatCppSdk/source/code/include/playfab/PlayFabEventRouter.h
+++ targets/XPlatCppSdk/source/code/include/playfab/PlayFabEventRouter.h
@@ -4,9 +4,19 @@
 
 #include <playfab/PlayFabEvent.h>
 #include <playfab/PlayFabEventPipeline.h>
+#include <playfab/OneDSEventPipeline.h>
 
 namespace PlayFab
 {
+    /// <summary>
+    /// The enumeration of all built-in event pipelines
+    /// </summary>
+    enum class EventPipelineKey
+    {
+        PlayFab, // PlayFab event pipeline
+        OneDS // OneDS (One Collector) event pipeline
+    };
+
     /// <summary>
     /// Interface for any event router
     /// </summary>
@@ -15,10 +25,10 @@ namespace PlayFab
     public:
         virtual ~IPlayFabEventRouter() {}
         virtual void RouteEvent(std::shared_ptr<const IPlayFabEmitEventRequest> request) const = 0; // Route an event to pipelines. This method must be thread-safe.
-        const std::unordered_map<size_t, std::shared_ptr<IPlayFabEventPipeline>>& GetPipelines() const;
+        const std::unordered_map<EventPipelineKey, std::shared_ptr<IPlayFabEventPipeline>>& GetPipelines() const;
 
     protected:
-        std::unordered_map<size_t, std::shared_ptr<IPlayFabEventPipeline>> pipelines;
+        std::unordered_map<EventPipelineKey, std::shared_ptr<IPlayFabEventPipeline>> pipelines;
     };
 
     /// <summary>
--- targets/XPlatCppSdk/source/code/source/playfab/PlayFabEventRouter.cpp
+++ targets/XPlatCppSdk/source/code/source/playfab/PlayFabEventRouter.cpp
@@ -6,22 +6,52 @@
 
 namespace PlayFab
 {
-    const std::unordered_map<size_t, std::shared_ptr<IPlayFabEventPipeline>>& IPlayFabEventRouter::GetPipelines() const
+    const std::unordered_map<EventPipelineKey, std::shared_ptr<IPlayFabEventPipeline>>& IPlayFabEventRouter::GetPipelines() const
     {
         return this->pipelines;
     }
 
     PlayFabEventRouter::PlayFabEventRouter()
     {
-        this->pipelines.emplace(0, std::move(std::shared_ptr<PlayFabEventPipeline>(new PlayFabEventPipeline(std::shared_ptr<PlayFabEventPipelineSettings>(new PlayFabEventPipelineSettings()))))); // add PlayFab pipeline by default
+        this->pipelines.emplace(EventPipelineKey::PlayFab, std::move(std::shared_ptr<PlayFabEventPipeline>(new PlayFabEventPipeline(std::shared_ptr<PlayFabEventPipelineSettings>(new PlayFabEventPipelineSettings()))))); // add PlayFab pipeline
+        this->pipelines.emplace(EventPipelineKey::OneDS, std::move(std::shared_ptr<OneDSEventPipeline>(new OneDSEventPipeline(std::shared_ptr<OneDSEventPipelineSettings>(new OneDSEventPipelineSettings()))))); // add OneDS pipeline
     }
 
     void PlayFabEventRouter::RouteEvent(std::shared_ptr<const IPlayFabEmitEventRequest> request) const
     {
-        // implement routing to all pipelines (by default)
-        for (auto& pipelineEntry : this->pipelines)
+        // only events based on PlayFabEmitEventRequest are supported by default pipelines
+        const PlayFabEmitEventRequest* pfRequestPtr = dynamic_cast<const PlayFabEmitEventRequest*>(request.get());
+        if (pfRequestPtr != nullptr)
         {
-            pipelineEntry.second->IntakeEvent(request);
+            for (auto& pipelineEntry : this->pipelines)
+            {
+                switch (pfRequestPtr->event->eventType)
+                {
+                    case PlayFabEventType::Default:
+                    case PlayFabEventType::Lightweight:
+                    {
+                        // route lightweight (and default) events to OneDS pipeline only
+                        if (pipelineEntry.first == EventPipelineKey::OneDS)
+                        {
+                            pipelineEntry.second->IntakeEvent(request);
+                        }
+                        break;
+                    }
+                    case PlayFabEventType::Heavyweight:
+                    {
+                        // route heavyweight events to PlayFab pipeline only
+                        if (pipelineEntry.first == EventPipelineKey::PlayFab)
+                        {
+                            pipelineEntry.second->IntakeEvent(request);
+                        }
+                        break;
+                    }
+                    default:
+                    {
+                        // do not route unsupported types of events
+                    }
+                }
+            }
         }
     }
 }

6e8f006e20dd331720458bcfe52fd5cf1d7ada34
Todd Bello <toddbell@microsoft.com>
2019-03-07 16:27:19 -0800

    ifdefing out www
    Removing the possibility of using www. We will need to update the documentation to reflect that PlayFabSharedSettings.Www will no longer be used after 2018.3 and instead will default to a UnityWebRequest.

--- targets/unity-v2/source/Shared/Public/PluginManager.cs
+++ targets/unity-v2/source/Shared/Public/PluginManager.cs
@@ -92,7 +92,11 @@ namespace PlayFab
 #endif
 #if UNITY_2017_2_OR_NEWER
             if (PlayFabSettings.RequestType == WebRequestType.UnityWww)
+#if !UNITY_2018_3_OR_NEWER // PlayFabWww will throw warnings as Unity has deprecated Www
+            {
                 transport = new PlayFabWww();
+            }
+#endif
 
             if (transport == null)
                 transport = new PlayFabUnityHttp();

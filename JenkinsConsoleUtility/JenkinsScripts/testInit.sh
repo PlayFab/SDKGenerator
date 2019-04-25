df9a81d489dcdfb98b79b62fadbdd07edd4c16a9
pgilmore <pagilmor@microsoft.com>
2019-04-25 14:43:48 -0700

    Testing Jenkins ability to verify a Gated Checkin

--- JenkinsConsoleUtility/JenkinsScripts/testInit.sh
+++ JenkinsConsoleUtility/JenkinsScripts/testInit.sh
@@ -11,44 +11,28 @@ CheckVerticalizedParameters
 CheckDefault SHARED_WORKSPACE C:/depot
 CheckDefault WORKSPACE C:/proj
 
-# USAGE: DelArcPatches <FullPath>
-DelArcPatches (){
-    echo === DelArcPatches $PWD, $@ ===
-    cd "$1"
-    echo Deleting Arc-Patches in: ${PWD}
-    git for-each-ref --format='%(refname:short)' refs/heads/arcpatch* | while read branch; do    BRANCH_EXISTS=$( git ls-remote --heads origin $branch | wc -l );    if [ $BRANCH_EXISTS -eq 0 ]; then        git branch -D $branch;    fi;done
-}
-
-# USAGE: ApplyArcPatch
-ApplyArcPatch (){
-    if [ -n "$PatchRepoName" ] && [ "$PatchRepoName" = "pf-main" ]; then
-        cd "$WORKSPACE/$PatchRepoName"
-        echo ==== arc patching pf-main ====
-        arc patch $DIFF_NUMBER --conduit-token $JENKINS_PHAB_TOKEN
-        echo ==== applyArcPatch Done ====
-    fi
-    if [ -n "$PatchRepoName" ] && [ "$PatchRepoName" = "SDKGenerator" ]; then
-        cd "$WORKSPACE/$PatchRepoName"
-        echo ==== arc patching SDKGenerator ====
-        arc patch $DIFF_NUMBER --conduit-token $JENKINS_PHAB_TOKEN
-        echo ==== applyArcPatch Done ====
-    fi
-    if [ -n "$PatchRepoName" ] && [ "$PatchRepoName" = "SdkName" ]; then
-        cd "$WORKSPACE/sdks/$PatchRepoName"
-        echo ==== arc patching $SdkName ====
-        arc patch $DIFF_NUMBER --conduit-token $JENKINS_PHAB_TOKEN
-        echo ==== applyArcPatch Done ====
-    fi
-}
-
-DoNugetWork (){
+DoJcuNugetUpdate (){
     pushd "$WORKSPACE/SDKGenerator/JenkinsConsoleUtility"
     cmd <<< "nuget restore JenkinsConsoleUtility.sln"
     popd
-    # None of the SDKs need pf-main directly
-    # pushd "$WORKSPACE/pf-main/Server"
-    # cmd <<< "nuget restore Server.sln"
-    # popd
+}
+
+# USAGE: ResetRepo
+ResetRepo (){
+    echo === ResetRepo $PWD, $@ ===
+
+    # Assumes the current directory is set to the repo to be reset
+    SetGitHubCreds
+    git fetch --progress origin
+    git checkout master || git checkout -b master || CleanCurrentRepo
+    git pull origin master
+
+    if [ "$GitDestBranch"!="master" ]; then
+        git fetch --progress origin
+        git branch -D $GitDestBranch || true
+        git checkout -b $GitDestBranch || true
+        git checkout $GitDestBranch
+    fi
 }
 
 # USAGE: DoWork
@@ -56,30 +40,16 @@ DoWork () {
     echo == DoWork $PWD, $@ ==
 
     # These are always shared, never modified directly, and never arc-patched
-    SyncGitHubRepo "$SHARED_WORKSPACE" "API_Specs"
-    # SyncGitHubRepo "$SHARED_WORKSPACE" "pf-main" # None of the SDKs need pf-main directly
-    SyncGitHubRepo "$SHARED_WORKSPACE" "SDKGenerator"
+    SyncGitHubRepo "$SHARED_WORKSPACE" "SDKGenerator" "SDKGenerator" "$SdkGenBranch"
     SyncGitHubRepo "$SHARED_WORKSPACE/sdks" "$SdkName"
-
-    # These can be arc-patched or modified
-    # TEMPORARY: API_Specs might already exist, without being a git repo because of previous script versions
-    rm -rf "$WORKSPACE/API_Specs"
-    SyncWorkspaceRepo "$SHARED_WORKSPACE" "$WORKSPACE" "API_Specs"
-    # SyncWorkspaceRepo "$SHARED_WORKSPACE" "$WORKSPACE" "pf-main" # None of the SDKs need pf-main directly
-    SyncWorkspaceRepo "$SHARED_WORKSPACE" "$WORKSPACE" "SDKGenerator"
+    SyncWorkspaceRepo "$SHARED_WORKSPACE" "$WORKSPACE" "SDKGenerator" "SDKGenerator" "$SdkGenBranch"
     SyncWorkspaceRepo "$SHARED_WORKSPACE/sdks" "$WORKSPACE/sdks" "$SdkName"
 
-    # It's always safe to remove past arc-patches
-    # DelArcPatches "$WORKSPACE/pf-main"
-    # DelArcPatches "$WORKSPACE/SDKGenerator"
-    # DelArcPatches "$WORKSPACE/sdks/$SdkName"
-
-    if [ -z "$quickTest" ]; then
-        DoNugetWork
-    else
-        echo SKIP: DoNugetWork for JCU
-    fi
-    # ApplyArcPatch
+    DoJcuNugetUpdate
+    
+    ForcePushD "$WORKSPACE/sdks/$SdkName"
+    ResetRepo
 }
 
+echo === Beginning testInit ===
 DoWork "$@"

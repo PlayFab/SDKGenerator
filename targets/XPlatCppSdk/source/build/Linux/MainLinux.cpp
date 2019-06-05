1640dd770b8c0869113f0773a24e1e330951e58b
jorgej <jorgej@dli.com>
2019-06-05 16:05:47 -0700

    Create TestApp for XPlat Linux build, fix it - unblock SSL-VERIFY bug (27957)

---
+++ targets/XPlatCppSdk/source/build/Linux/MainLinux.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+
+#include "TestApp.h"
+
+// Win32 Entry Point
+int main()
+{
+    PlayFabUnit::TestApp testApp;
+
+    int result = testApp.Main();
+    return result;
+}

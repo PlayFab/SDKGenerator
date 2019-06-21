b546d2a0c6695298ba769ecd04774f6aa955fc2e
MichaelMackendMS <michael.mackend@microsoft.com>
2019-06-21 10:42:22 -0700

    add xplat android build script

---
+++ SDKBuildScripts/xplat_BuildAndTestAndroid.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+pushd ../../sdks/XPlatCppSdk/build/Android
+AndroidProjectPath=$PWD
+popd
+
+apkPath=$AndroidProjectPath/app/build/outputs/apk/debug/app-debug.apk
+testAssemblyDir="$1"
+
+CopyTestTitleData() {
+    cp -f "$PF_TEST_TITLE_DATA_JSON" "$AndroidProjectPath/app/src/main/assets"
+}
+
+BuildAPK() {
+    pushd "$AndroidProjectPath"
+    ./gradlew assembleDebug
+    popd
+}
+
+TestAPK() {
+    appcenter test run uitest --app "PlayFabSDKTeam/PlayFabXPlatAndroid" \
+    --devices ce2f4064 \
+    --app-path "$apkPath"  \
+    --test-series "master" \
+    --locale "en_US" \
+    --assembly-dir "$testAssemblyDir"  \
+    --uitest-tools-dir "$XAMARIN_UITEST_TOOLS"
+}
+
+DoWork() {
+    CopyTestTitleData
+    BuildAPK
+    TestAPK
+}
+
+DoWork

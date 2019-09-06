ade8b030b5ec3070880af44151fba51c87a0fc42
pgilmorepf <paul@playfab.com>
2019-09-06 15:20:00 -0700

    More values from inputs rather than hard coded consts.

--- JenkinsConsoleUtility/jcuSrc/Commands/CheckBblStandby.cs
+++ JenkinsConsoleUtility/jcuSrc/Commands/CheckBblStandby.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using JenkinsConsoleUtility.Util;
 using PlayFab;
 using PlayFab.MultiplayerModels;
-using System.Windows;
+using System.Linq;
 
 namespace JenkinsConsoleUtility.Commands
 {
@@ -24,21 +24,21 @@ namespace JenkinsConsoleUtility.Commands
 
         private static readonly string[] MyCommandKeys = { "CheckBbl", "BblStandby" };
         public string[] CommandKeys => MyCommandKeys;
-        private static readonly string[] MyMandatoryArgKeys = { "BBL_VERSIONS" };
-        public string[] MandatoryArgKeys => MyMandatoryArgKeys;
+        public string[] MandatoryArgKeys => null;
 
         private PlayFabAuthenticationContext context;
         private PlayFabApiSettings settings;
         private PlayFabAuthenticationInstanceAPI authApi;
         private PlayFabMultiplayerInstanceAPI multiplayerApi;
 
-        private readonly TimeSpan cycleDuration = TimeSpan.FromMinutes(0.5);
-        private readonly TimeSpan cyclePeriod = TimeSpan.FromSeconds(10);
-        private readonly TimeSpan retryDelay = TimeSpan.FromSeconds(10);
-        private const int retryCount = 5;
-        private readonly int[] GAP_THRESHOLDS = new[] { 10000, 10000, 1000, 400, 200, 100 };
-        private readonly int[] MAX_PERCENT_THRESHOLDS = new[] { 1000, 1000, 100, 95, 80, 75 };
-        private readonly int[] STANDBY_THRESHOLDS = new[] { -1, -1, 0, 6, 9, 11 };
+        // "Constants" defined by program inputs (vars or cmd-line)
+        private TimeSpan CYCLE_DURATION_MINS = TimeSpan.FromMinutes(10);
+        private TimeSpan CYCLE_PERIOD_SEC = TimeSpan.FromSeconds(30);
+        private TimeSpan RETRY_DELAY_SEC = TimeSpan.FromSeconds(10);
+        private int RETRY_COUNT = 5;
+        private int[] GAP_THRESHOLDS = new[] { 10000, 10000, 1000, 400, 200, 100 };
+        private int[] MAX_CAPACITY_PERCENT_THRESHOLDS = new[] { 1000, 1000, 100, 95, 80, 75 };
+        private int[] STANDBY_THRESHOLDS = new[] { -1, -1, 0, 6, 9, 11 };
 
         private DateTime endOfCycle;
         private string bblVersions;
@@ -55,7 +55,7 @@ namespace JenkinsConsoleUtility.Commands
 
         public int Execute(Dictionary<string, string> argsLc, Dictionary<string, string> argsCased)
         {
-            bblVersions = JenkinsConsoleUtility.GetArgVar(argsLc, "BBL_VERSIONS");
+            ParseInputs(argsLc);
 
             var setupCode = LoginToPlayfab(argsLc);
             if (setupCode != 0)
@@ -63,7 +63,7 @@ namespace JenkinsConsoleUtility.Commands
 
             var cumulativeResult = new Tuple<int, Severity>(0, Severity.SEV_0);
 
-            endOfCycle = DateTime.UtcNow + cycleDuration;
+            endOfCycle = DateTime.UtcNow + CYCLE_DURATION_MINS;
             while (endOfCycle > DateTime.UtcNow)
             {
                 var eachSummary = GetBuildSummaryFromPlayfab();
@@ -75,7 +75,7 @@ namespace JenkinsConsoleUtility.Commands
                     (Severity)Math.Max((byte)cumulativeResult.Item2, (byte)eachResult.Item2) // Least Error Seen
                 );
 
-                Thread.Sleep((int)cyclePeriod.TotalMilliseconds);
+                Thread.Sleep((int)CYCLE_PERIOD_SEC.TotalMilliseconds);
             }
 
             JcuUtil.FancyWriteToConsole("Finished Query Cycle");
@@ -84,6 +84,28 @@ namespace JenkinsConsoleUtility.Commands
             MakeAlert(null, cumulativeResult.Item2, " <- Typical Error Level. Total Number of Alerts: " + cumulativeResult.Item1);
 
             return (cumulativeResult.Item2 <= Severity.SEV_3) ? cumulativeResult.Item1 : 0;
+        }
+
+        private void ParseInputs(Dictionary<string, string> argsLc)
+        {
+            if (!JenkinsConsoleUtility.TryGetArgVar(out bblVersions, argsLc, "BBL_VERSIONS"))
+                bblVersions = "3.0.0;2.0.1";
+
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempGapThresholds, argsLc, "GAP_THRESHOLDS"))
+                try { GAP_THRESHOLDS = tempGapThresholds.Split(';').Select(s => int.Parse(s)).ToArray(); } catch (Exception) { }
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempStandby, argsLc, "STANDBY_THRESHOLDS"))
+                try { STANDBY_THRESHOLDS = tempGapThresholds.Split(';').Select(s => int.Parse(s)).ToArray(); } catch (Exception) { }
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempMaxCap, argsLc, "MAX_CAPACITY_PERCENT_THRESHOLDS"))
+                try { MAX_CAPACITY_PERCENT_THRESHOLDS = tempGapThresholds.Split(';').Select(s => int.Parse(s)).ToArray(); } catch (Exception) { }
+
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempCycleDuration, argsLc, "CYCLE_DURATION_MINS"))
+                try { CYCLE_DURATION_MINS = TimeSpan.FromMinutes(double.Parse(tempCycleDuration)); } catch (Exception) { }
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempCyclePeriod, argsLc, "CYCLE_PERIOD_SEC"))
+                try { CYCLE_PERIOD_SEC = TimeSpan.FromSeconds(double.Parse(tempCyclePeriod)); } catch (Exception) { }
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempRetryDelay, argsLc, "RETRY_DELAY_SEC"))
+                try { RETRY_DELAY_SEC = TimeSpan.FromSeconds(double.Parse(tempRetryDelay)); } catch (Exception) { }
+            if (JenkinsConsoleUtility.TryGetArgVar(out string tempRetryCount, argsLc, "RETRY_COUNT"))
+                try { RETRY_COUNT = int.Parse(tempRetryCount); } catch (Exception) { }
         }
 
         private int LoginToPlayfab(Dictionary<string, string> argsLc)
@@ -107,7 +129,7 @@ namespace JenkinsConsoleUtility.Commands
             settings.DeveloperSecretKey = testTitleData.developerSecretKey;
 
             // Perform PlayFab Login
-            for (int i = 0; i < retryCount; i++)
+            for (int i = 0; i < RETRY_COUNT; i++)
             {
                 var tokenTask = authApi.GetEntityTokenAsync(null);
                 tokenTask.Wait();
@@ -115,7 +137,7 @@ namespace JenkinsConsoleUtility.Commands
                 if (string.IsNullOrEmpty(context.EntityToken))
                 {
                     JcuUtil.FancyWriteToConsole(ConsoleColor.Red, "ERROR: CheckBblStandby was not able to authenticate as expected, sleeping...");
-                    Thread.Sleep((int)retryDelay.TotalMilliseconds);
+                    Thread.Sleep((int)RETRY_DELAY_SEC.TotalMilliseconds);
                 }
                 else
                 {
@@ -136,7 +158,7 @@ namespace JenkinsConsoleUtility.Commands
         {
             var listBuildsRequest = new ListBuildSummariesRequest { PageSize = 100 };
 
-            for (int i = 0; i < retryCount; i++)
+            for (int i = 0; i < RETRY_COUNT; i++)
             {
                 var listBuildsTask = multiplayerApi.ListBuildSummariesAsync(listBuildsRequest);
                 listBuildsTask.Wait();
@@ -144,7 +166,7 @@ namespace JenkinsConsoleUtility.Commands
                 if (buildSummaries == null)
                 {
                     JcuUtil.FancyWriteToConsole(ConsoleColor.Red, "ERROR: Was unable to list build summaries, sleeping...");
-                    Thread.Sleep((int)retryDelay.TotalMilliseconds);
+                    Thread.Sleep((int)RETRY_DELAY_SEC.TotalMilliseconds);
                 }
                 else
                 {
@@ -201,11 +223,11 @@ namespace JenkinsConsoleUtility.Commands
                         }
                     }
 
-                    for (var i = 0; i < MAX_PERCENT_THRESHOLDS.Length; i++)
+                    for (var i = 0; i < MAX_CAPACITY_PERCENT_THRESHOLDS.Length; i++)
                     {
-                        if (each.CurrentServerStats.Active >= (each.MaxServers * MAX_PERCENT_THRESHOLDS[i] / 100))
+                        if (each.CurrentServerStats.Active >= (each.MaxServers * MAX_CAPACITY_PERCENT_THRESHOLDS[i] / 100))
                         {
-                            MakeAlert(bblAlerts, (Severity)i, versionString + ", " + eachSummary.BuildId + " - " + MAX_PERCENT_THRESHOLDS[i] + "% (" + each.CurrentServerStats.Active + "/" + each.MaxServers + ") Max Servers reached in region:" + each.Region);
+                            MakeAlert(bblAlerts, (Severity)i, versionString + ", " + eachSummary.BuildId + " - " + MAX_CAPACITY_PERCENT_THRESHOLDS[i] + "% (" + each.CurrentServerStats.Active + "/" + each.MaxServers + ") Max Servers reached in region:" + each.Region);
                             worstBblSeverity = (Severity)Math.Min((byte)worstBblSeverity, i);
                             worstTickSeverity = (Severity)Math.Min((byte)worstTickSeverity, i);
                             break;

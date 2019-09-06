7ac8c51575851e53fec28102cb3f1a2b7c94b260
pgilmorepf <paul@playfab.com>
2019-09-06 14:14:36 -0700

    More alerts, and preping for a 10-min loop

--- JenkinsConsoleUtility/jcuSrc/Commands/CheckBblStandby.cs
+++ JenkinsConsoleUtility/jcuSrc/Commands/CheckBblStandby.cs
@@ -8,6 +8,18 @@ namespace JenkinsConsoleUtility.Commands
 {
     public class CheckBblStandby : ICommand
     {
+        private enum Severity
+        {
+            SEV_0, // Wake up everyone
+            SEV_1, // Wake up team
+            SEV_2, // Wake up on-call
+            SEV_3, // Send an email
+            SEV_4, // File a bug
+            SEV_5, // Follow up if there's lots of them
+
+            NONE, // No issue
+        }
+
         private static readonly string[] MyCommandKeys = { "CheckBbl", "BblStandby" };
         public string[] CommandKeys => MyCommandKeys;
         private static readonly string[] MyMandatoryArgKeys = { "BBL_VERSIONS" };
@@ -18,11 +30,9 @@ namespace JenkinsConsoleUtility.Commands
         private PlayFabAuthenticationInstanceAPI authApi;
         private PlayFabMultiplayerInstanceAPI multiplayerApi;
 
-        private const int S2_MAX_THRESHHOLD_PERCENT = 90;
-        private const int S3_MAX_THRESHHOLD_PERCENT = 75;
-
-        private const int S1_LOW_STANDBY_THRESHHOLD = 3;
-        private const int S2_LOW_STANDBY_THRESHHOLD = 11;
+        private readonly int[] GAP_THRESHOLDS = new[] { 10000, 10000, 1000, 400, 200, 100 };
+        private readonly int[] MAX_PERCENT_THRESHOLDS = new[] { 1000, 1000, 100, 95, 80, 75 };
+        private readonly int[] STANDBY_THRESHOLDS = new[] { -1, -1, 0, 6, 9, 11 };
 
         public CheckBblStandby()
         {
@@ -71,8 +81,18 @@ namespace JenkinsConsoleUtility.Commands
                 return 1;
             }
 
-            List<string> alerts = new List<string>();
+            var buildResults = EvaluateBuildSummaries(bblVersions, buildSummaries);
+
+            return (buildResults.Item2 <= Severity.SEV_3) ? buildResults.Item1 : 0;
+        }
+
+        private Tuple<int, Severity> EvaluateBuildSummaries(string bblVersions, List<BuildSummary> buildSummaries)
+        {
+            List<string> bblAlerts = new List<string>();
+            List<string> thAlerts = new List<string>();
             HashSet<string> completedVersions = new HashSet<string>();
+
+            Severity worstSeverity = Severity.NONE;
             foreach (var eachSummary in buildSummaries)
             {
                 if (!eachSummary.Metadata.TryGetValue("Version", out string versionString))
@@ -83,49 +103,81 @@ namespace JenkinsConsoleUtility.Commands
                 bool isDeployed = true;
                 foreach (var eachRegionConfig in eachSummary.RegionConfigurations)
                     isDeployed &= eachRegionConfig.Status == "Deployed";
-                if (!isDeployed)
+                if (!isDeployed || eachSummary.RegionConfigurations.Count == 0)
                     continue;
-
-                Action<string> makeAlert = (msg) =>
-                {
-                    ConsoleColor alertColor;
-                    switch (msg.Substring(0, 4))
-                    {
-                        case "SEV1": alertColor = ConsoleColor.Magenta; break;
-                        case "SEV2": alertColor = ConsoleColor.Red; break;
-                        case "SEV3": alertColor = ConsoleColor.Yellow; break;
-                        default: alertColor = ConsoleColor.White; break;
-                    }
-                    JcuUtil.FancyWriteToConsole(alertColor, msg);
-                    alerts.Add(msg);
-                };
 
                 JcuUtil.FancyWriteToConsole("Found Version: " + versionString + ",  BuildId: " + eachSummary.BuildId);
                 foreach (var each in eachSummary.RegionConfigurations)
                 {
-                    JcuUtil.FancyWriteToConsole(" - " + each.Region + ", (sby:" + each.CurrentServerStats.StandingBy + "/prp:" + each.CurrentServerStats.Propping + "/act:" + each.CurrentServerStats.Active + "/max:" + each.MaxServers + ")");
+                    JcuUtil.FancyWriteToConsole(" - " + each.Region + ", (sby:" + each.StandbyServers + "/max:" + each.MaxServers + ")(sby:" + each.CurrentServerStats.StandingBy + "/prp:" + each.CurrentServerStats.Propping + "/act:" + each.CurrentServerStats.Active + "/max:" + each.MaxServers + ")");
 
-                    if (each.CurrentServerStats.Active >= each.MaxServers)
-                        makeAlert("SEV1: " + versionString + ", " + eachSummary.BuildId + " - Max Servers reached in region:" + each.Region);
-                    else if (each.CurrentServerStats.Active >= (each.MaxServers * S2_MAX_THRESHHOLD_PERCENT / 100))
-                        makeAlert("SEV2: " + versionString + ", " + eachSummary.BuildId + " - " + S2_MAX_THRESHHOLD_PERCENT + "% (" + each.CurrentServerStats.Active + "/" + each.MaxServers + ") Max Servers reached in region:" + each.Region);
-                    else if (each.CurrentServerStats.Active >= (each.MaxServers * S3_MAX_THRESHHOLD_PERCENT / 100))
-                        makeAlert("SEV3: " + versionString + ", " + eachSummary.BuildId + " - " + S3_MAX_THRESHHOLD_PERCENT + "% (" + each.CurrentServerStats.Active + "/" + each.MaxServers + ") Max Servers reached in region:" + each.Region);
+                    if (each.CurrentServerStats.StandingBy == 0 && each.CurrentServerStats.Active == 0 && each.CurrentServerStats.Propping == 0)
+                    {
+                        MakeAlert(thAlerts, Severity.SEV_4, "Bad region numbers detected");
+                        worstSeverity = Severity.SEV_4;
+                        continue; // All hell will break loose below, with these numbers, but they're not legit, so skip it
+                    }
 
-                    if (each.CurrentServerStats.StandingBy <= S1_LOW_STANDBY_THRESHHOLD)
-                        makeAlert("SEV1: " + versionString + ", " + eachSummary.BuildId + " - Standby == 0 for region:" + each.Region + ", " + each.CurrentServerStats.StandingBy + "<=" + S1_LOW_STANDBY_THRESHHOLD);
-                    else if (each.CurrentServerStats.StandingBy <= S2_LOW_STANDBY_THRESHHOLD)
-                        makeAlert("SEV2: " + versionString + ", " + eachSummary.BuildId + " - Low Standby in region:" + each.Region + ", " + each.CurrentServerStats.StandingBy + "<=" + S2_LOW_STANDBY_THRESHHOLD);
+                    var gap = each.StandbyServers - each.CurrentServerStats.StandingBy - each.CurrentServerStats.Propping;
+                    for (var i = 0; i < GAP_THRESHOLDS.Length; i++)
+                    {
+                        if (gap >= GAP_THRESHOLDS[i])
+                        {
+                            MakeAlert(thAlerts, (Severity)i, versionString + ", " + eachSummary.BuildId + " - High (StandBy + Propping) Gap in region:" + gap);
+                            worstSeverity = (Severity)i;
+                            break;
+                        }
+                    }
+
+                    for (var i = 0; i < MAX_PERCENT_THRESHOLDS.Length; i++)
+                    {
+                        if (each.CurrentServerStats.Active >= (each.MaxServers * MAX_PERCENT_THRESHOLDS[i] / 100))
+                        {
+                            MakeAlert(bblAlerts, (Severity)i, versionString + ", " + eachSummary.BuildId + " - " + MAX_PERCENT_THRESHOLDS[i] + "% (" + each.CurrentServerStats.Active + "/" + each.MaxServers + ") Max Servers reached in region:" + each.Region);
+                            worstSeverity = (Severity)i;
+                            break;
+                        }
+                    }
+
+                    for (var i = 0; i < STANDBY_THRESHOLDS.Length; i++)
+                    {
+                        if (each.CurrentServerStats.StandingBy <= STANDBY_THRESHOLDS[i])
+                        {
+                            MakeAlert(bblAlerts, (Severity)i, versionString + ", " + eachSummary.BuildId + " - Low Standby in region:" + each.Region + ", " + each.CurrentServerStats.StandingBy + "<=" + STANDBY_THRESHOLDS[i]);
+                            worstSeverity = (Severity)i;
+                            break;
+                        }
+                    }
                 }
 
                 JcuUtil.FancyWriteToConsole(null);
                 completedVersions.Add(versionString);
             }
 
-            if (alerts.Count > 0)
-                JcuUtil.FancyWriteToConsole(ConsoleColor.Yellow, "Alert list:", ConsoleColor.Red, null, alerts, null);
+            if (bblAlerts.Count > 0)
+                JcuUtil.FancyWriteToConsole(ConsoleColor.Yellow, "Bumblelion alert list:", ConsoleColor.Red, null, bblAlerts, null);
+            if (thAlerts.Count > 0)
+                JcuUtil.FancyWriteToConsole(ConsoleColor.Yellow, "Thunderhead alert list:", ConsoleColor.Blue, null, thAlerts, null);
 
-            return alerts.Count;
+            return new Tuple<int, Severity>(bblAlerts.Count + thAlerts.Count, worstSeverity);
+        }
+
+        private void MakeAlert(List<string> alertList, Severity severity, string msg)
+        {
+            ConsoleColor alertColor;
+            switch (severity)
+            {
+                case Severity.SEV_0: alertColor = ConsoleColor.Magenta; break;
+                case Severity.SEV_1: alertColor = ConsoleColor.Magenta; break;
+                case Severity.SEV_2: alertColor = ConsoleColor.Red; break;
+                case Severity.SEV_3: alertColor = ConsoleColor.Yellow; break;
+                case Severity.SEV_4: alertColor = ConsoleColor.Yellow; break;
+                case Severity.SEV_5: alertColor = ConsoleColor.DarkGray; break;
+                default: alertColor = ConsoleColor.White; break;
+            }
+            JcuUtil.FancyWriteToConsole(alertColor, severity + " " + msg);
+            if (severity <= Severity.SEV_4)
+                alertList.Add(msg);
         }
     }
 }

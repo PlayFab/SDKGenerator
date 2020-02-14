88f721d19ed01e9f44054ebe78e96cda91d1557a
pgilmorepf <paul@playfab.com>
2020-02-13 18:12:00 -0800

    Removing retry on step that doesn't benefit from it.

--- SDKBuildScripts/xamarin_buildAppCenterTestIOS.sh
+++ SDKBuildScripts/xamarin_buildAppCenterTestIOS.sh
@@ -170,7 +170,7 @@ ExtractBuildResults() {
 DownloadIpa() {
     shopt -s nocasematch
     if [[ $BuildResult == *"succeeded"* ]]; then
-        RetryLoop appcenter build download --type build --app "$PlayFabApplicationName" --id $BuildNumber --file PlayFabIOS.ipa
+        appcenter build download --type build --app "$PlayFabApplicationName" --id $BuildNumber --file PlayFabIOS.ipa
         ExitIfError
     else
         appcenter build logs --app "$PlayFabApplicationName" --id $BuildNumber >> "build_logs_${BuildNumber}.txt"

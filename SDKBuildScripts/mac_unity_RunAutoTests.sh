1b181fcb09ae64f45de04bd938194e725ad74c78
Todd Bello <toddbell@microsoft.com>
2020-02-20 16:20:49 -0800

    Update mac_unity_RunAutoTests.sh
    Cleaning up crufty comments

--- SDKBuildScripts/mac_unity_RunAutoTests.sh
+++ SDKBuildScripts/mac_unity_RunAutoTests.sh
@@ -43,28 +43,22 @@ fi
 
 echo current installed unity version - $CurrentInstalledUnityVer
 
-# TODO: we need specific versions of Unity targeted below. Right now we are hard coded to 2019.1.3 (this may be problematic to upgrades)
-
 RepoProject="${WORKSPACE}/sdks/${SdkName}/ExampleTestProject"
-ProjRootPath="${WORKSPACE}/${UNITY_VERSION}"
 BuildIdentifier=JBuild_${SdkName}_${VerticalName}_${NODE_NAME}_${EXECUTOR_NUMBER}
 
 JenkernaughtSaveCloudScriptResults() {
     echo === Save test results to Jenkernaught ===
     pushd "${WORKSPACE}/SDKGenerator/JenkinsConsoleUtility/bin/Debug"
     mono JenkinsConsoleUtility.exe --listencs -buildIdentifier $BuildIdentifier -workspacePath ${WORKSPACE} -timeout 30 -verbose true
-    # . ./JenkinsConsoleUtility --listencs -buildIdentifier $BuildIdentifier -workspacePath ${WORKSPACE} -timeout 30 -verbose true
     popd
 }
 
 RunMacJenkernaught() {
     echo === Build OSX Client Target ===
     pushd "${RepoProject}/"
-    #/Applications/Unity/Hub/Editor/2019.1.3f1/Unity.app/Contents/MacOS/Unity -projectPath "${RepoProject}" -buildOSXUniversalPlayer "${ProjRootPath}/${SdkName}_TC" -accept-apiupdate -disable-assembly-updater -noUpm -nographics -quit -batchmode -executeMethod PlayFab.Internal.PlayFabPackager.MakeOsxBuild -logFile "${WORKSPACE}/logs/buildOSXClient.txt" || (cat "${WORKSPACE}/logs/buildOSXClient.txt" && return 1)
     /Applications/Unity/Hub/Editor/"${CurrentInstalledUnityVer}"/Unity.app/Contents/MacOS/Unity -projectPath "${RepoProject}" -buildOSXUniversalPlayer "${RepoProject}/testBuilds/PlayFabOSX" -accept-apiupdate -disable-assembly-updater -noUpm -nographics -quit -batchmode -executeMethod PlayFab.Internal.PlayFabPackager.MakeOsxBuild -logFile "${WORKSPACE}/logs/buildMacClient.txt" || (cat "${WORKSPACE}/logs/buildMacClient.txt") || true
     popd
     pushd "${WORKSPACE}/testBuilds/PlayFabOSX/"
-    #chmod +x "PlayFabOSX.app"
     open PlayFabOSX.app
     popd
     JenkernaughtSaveCloudScriptResults

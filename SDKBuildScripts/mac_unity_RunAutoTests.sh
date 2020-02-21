47efdcd2d21ddf8ee62f69c8485d21d08b1a2350
Todd Bello <toddbell@microsoft.com>
2020-02-21 09:39:44 -0800

    Toddbell/unity mac script updates (#664)
    * Updating mac build scripts for unity
    
    we made some directory changes that need to be reflected in the scripts
    
    * Update mac_unity_RunAutoTests.sh
    
    attempting to call Unity directly by its mac default install path
    
    * Update mac_unity_RunAutoTests.sh
    
    unity is stored as a .app file on a mac's Applications folder
    
    * Update mac_unity_RunAutoTests.sh
    
    Unity.app acts as a directory on macOS and not an exe. The actual file to run Unity is in a deeper folder.
    
    * small notes and quickfixes
    
    we are hard coding unity versions for the moment, also double checking Asset folder path for test scenes (may need to add the ExampleProject\ prefix)
    
    * Update mac_unity_RunAutoTests.sh
    
    explicitly define a scene for unity to pick up
    
    * Update PlayFabPackager.cs
    
    testing out universal build target (which seems to be the newer/supported build target?)
    
    * Update mac_unity_RunAutoTests.sh
    
    path shouldn't be required like other platforms.
    
    * Update PlayFabPackager.cs
    
    reverting something that didn't seem to affect the build
    
    * Update mac_unity_RunAutoTests.sh
    
    mac didn't explicitly call out a projectPath?
    
    * Update mac_unity_SetupTestProjects.sh
    
    Quick TC removal to reflect changes made in windows
    
    * Update mac_unity_RunAutoTests.sh
    
    Fixing comment
    
    * Update mac_unity_RunAutoTests.sh
    
    pulled a run command from the other Unity_RunAutoTests.sh file
    
    * Update mac_unity_RunAutoTests.sh
    
    run the exe without a cmd required (doesn't work on a mac)
    
    * Update mac_unity_RunAutoTests.sh
    
    stop and exit at failure
    
    * Update mac_unity_RunAutoTests.sh
    
    We are building the Unity project, but we never ran it? This should at least build a standalone version of the app?
    
    * Update mac_unity_RunAutoTests.sh
    
    Yeah building the player seems to have broken something else.
    
    * Update mac_unity_RunAutoTests.sh
    
    explicitly open the built .app file
    
    * Update mac_unity_RunAutoTests.sh
    
    Yeah that buildPlayer was required.
    
    * Update mac_unity_RunAutoTests.sh
    
    oh don't kill the job because we are outputting this. It may be a noisy false alarm.
    
    * Update mac_unity_RunAutoTests.sh
    
    REALLY make sure this line goes though
    
    * Update mac_unity_RunAutoTests.sh
    
    different build folder apparently
    
    * Update mac_unity_RunAutoTests.sh
    
    attempting to make the UnityVersion a more flexible parameter
    
    * Update mac_unity_RunAutoTests.sh
    
    Updating latest Unity versions on the mac node
    
    * Update mac_unity_RunAutoTests.sh
    
    Cleaning up crufty comments
    
    * Delete mac_unity_SetupTestProjects.sh
    
    This script is no longer called or needed.
    
    * Update mac_unity_RunAutoTests.sh
    
    adding notes about unity mac version issues. default to known install and optionally switch to legacy supported versions also known to be installed.

--- SDKBuildScripts/mac_unity_RunAutoTests.sh
+++ SDKBuildScripts/mac_unity_RunAutoTests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # USAGE: unity_RunAutoTests.bat [<UNITY_VERSION>] [<SdkName>]
 
 if [ -z $1 ]; then
@@ -13,24 +15,56 @@ else
     SdkName=$2
 fi
 
+CurrentInstalledUnityVer=""
+
+# Bug 41579: install up to date Unity on mac
+# for now we will default to a known version install and swap to either 2018 or 2017 if requested.
+#if [$UNITY_VERSION="UNITY201"]; then
+#    CurrentInstalledUnityVer=2020.1.0a23
+#fi
+
+#if [$UNITY_VERSION=="UNITY193" ]; then
+#    CurrentInstalledUnityVer=2019.3.2f1
+#fi
+
+#if [$UNITY_VERSION=="UNITY192" ]; then
+#    CurrentInstalledUnityVer=2019.2.21f1
+#fi
+
+#if [$UNITY_VERSION=="UNITY191" ]; then
+    CurrentInstalledUnityVer=2019.1.3f1
+#fi
+
+if [$UNITY_VERSION="UNITY184"]; then
+#    CurrentInstalledUnityVer=2018.4.17f1
+     CurrentInstalledUnityVer=2018.4.0f1
+fi
+
+if [UNITY_VERSION="UNITY174"]; then
+#    CurrentInstalledUnityVer=2017.4.37f1
+    CurrentInstalledUnityVer=2018.4.27f1
+fi
+
+echo current installed unity version - $CurrentInstalledUnityVer
+
 RepoProject="${WORKSPACE}/sdks/${SdkName}/ExampleTestProject"
-ProjRootPath="${WORKSPACE}/${UNITY_VERSION}"
 BuildIdentifier=JBuild_${SdkName}_${VerticalName}_${NODE_NAME}_${EXECUTOR_NUMBER}
 
 JenkernaughtSaveCloudScriptResults() {
     echo === Save test results to Jenkernaught ===
     pushd "${WORKSPACE}/SDKGenerator/JenkinsConsoleUtility/bin/Debug"
-    cmd <<< "JenkinsConsoleUtility --listencs -buildIdentifier $BuildIdentifier -workspacePath ${WORKSPACE} -timeout 30 -verbose true"
-    # . ./JenkinsConsoleUtility --listencs -buildIdentifier $BuildIdentifier -workspacePath ${WORKSPACE} -timeout 30 -verbose true
+    mono JenkinsConsoleUtility.exe --listencs -buildIdentifier $BuildIdentifier -workspacePath ${WORKSPACE} -timeout 30 -verbose true
     popd
 }
 
 RunMacJenkernaught() {
     echo === Build OSX Client Target ===
-    pushd "${ProjRootPath}/${SdkName}_TC/"
-    $UNITY_VERSION -buildOSXUniversalPlayer "${ProjRootPath}/${SdkName}_TC" -accept-apiupdate -disable-assembly-updater -noUpm -nographics -quit -batchmode -executeMethod PlayFab.Internal.PlayFabPackager.MakeOsxBuild -logFile "${WORKSPACE}/logs/buildOSXClient.txt" || (cat "${WORKSPACE}/logs/buildOSXClient.txt" && return 1)
+    pushd "${RepoProject}/"
+    /Applications/Unity/Hub/Editor/"${CurrentInstalledUnityVer}"/Unity.app/Contents/MacOS/Unity -projectPath "${RepoProject}" -buildOSXUniversalPlayer "${RepoProject}/testBuilds/PlayFabOSX" -accept-apiupdate -disable-assembly-updater -noUpm -nographics -quit -batchmode -executeMethod PlayFab.Internal.PlayFabPackager.MakeOsxBuild -logFile "${WORKSPACE}/logs/buildMacClient.txt" || (cat "${WORKSPACE}/logs/buildMacClient.txt") || true
     popd
-
+    pushd "${WORKSPACE}/testBuilds/PlayFabOSX/"
+    open PlayFabOSX.app
+    popd
     JenkernaughtSaveCloudScriptResults
     if [[ $? -ne 0 ]]; then return 1; fi
 }

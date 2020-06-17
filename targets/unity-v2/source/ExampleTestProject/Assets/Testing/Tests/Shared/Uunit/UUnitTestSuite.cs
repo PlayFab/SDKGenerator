941ba694d86f10337f4391d942a7ee14c0ed141a
Todd Bello <toddbell@microsoft.com>
2020-06-17 14:57:07 -0700

    Update UUnitTestSuite.cs
    removing commented out code that is no longer needed

--- targets/unity-v2/source/ExampleTestProject/Assets/Testing/Tests/Shared/Uunit/UUnitTestSuite.cs
+++ targets/unity-v2/source/ExampleTestProject/Assets/Testing/Tests/Shared/Uunit/UUnitTestSuite.cs
@@ -325,7 +325,6 @@ namespace PlayFab.UUnit
             testContext.EndTime = now;
             Wrap(testContext, testContext.TestInstance.TearDown);
             testContext.ActiveState = UUnitActiveState.COMPLETE;
-            //_testReport.TestComplete(testContext.TestDelegate.Target.GetType().Name + "." + testContext.Name, testContext.FinishState, (int)(testContext.EndTime - testContext.StartTime).TotalMilliseconds, testContext.TestResultMsg, null);
         }
     }
 }

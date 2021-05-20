a063b252e47dcfaa73e66c13f567f5150ceb7c45
Todd Bello <toddbell@microsoft.com>
2021-05-20 14:52:42 -0700

    Update make.js

--- targets/LuaSdk/make.js
+++ targets/LuaSdk/make.js
@@ -177,15 +177,16 @@ function getResultAction(tabbing, apiCall) {
     if (apiCall.url === "/Authentication/GetEntityToken")
         preCallback = internalTabbing + "PlayFabSettings._internalSettings.entityToken = result.EntityToken\n";
     else if (apiCall.result === "LoginResult") {
-        preCallback = internalTabbing + "print(\"--- LoginResult called --- \")\n"
+        preCallback = "" //internalTabbing + "print(\"--- LoginResult called --- \")\n"
             + internalTabbing + "PlayFabSettings._internalSettings.sessionTicket = result.SessionTicket\n"
             + internalTabbing + "if (result.EntityToken) then \n"
-            + internalTabbing + internalTabbing + "print(\"--- Entity Token was present, copy to _internalSetting ---\")\n";
+            //+ internalTabbing + internalTabbing + "print(\"--- Entity Token was present, copy to _internalSetting ---\")\n";
             + internalTabbing + internalTabbing + "PlayFabSettings._internalSettings.entityToken = result.EntityToken.EntityToken\n";
             + internalTabbing + "end\n";
     }
     else if (apiCall.request === "RegisterPlayFabUserRequest") {
-        preCallback = internalTabbing + "print(\"--- Detected Registering Play Fab User Request ---\")\n"
+        preCallback = "" 
+            // internalTabbing + "print(\"--- Detected Registering Play Fab User Request ---\")\n"
             + internalTabbing + "PlayFabSettings._internalSettings.sessionTicket = result.SessionTicket\n";
     }
 
@@ -195,7 +196,7 @@ function getResultAction(tabbing, apiCall) {
             + tabbing + "function wrappedOnSuccess(result)\n"
             + preCallback
             + tabbing + "    if (externalOnSuccess) then\n"
-            + tabbing + "        print(\"--- Login Result On Success Called Back! ---\")\n"
+            //+ tabbing + "        print(\"--- Login Result On Success Called Back! ---\")\n"
             + tabbing + "        externalOnSuccess(result)\n"
             + tabbing + "    end\n"
             + tabbing + "end\n";

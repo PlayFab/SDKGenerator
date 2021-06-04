a88bda160576241418a2837f449ea8914361afb9
Todd Bello <toddbell@microsoft.com>
2021-06-03 17:30:10 -0700

    Update PlayFabUnityHttp.cs

--- targets/unity-v2/source/ExampleTestProject/Assets/PlayFabSDK/Shared/Internal/PlayFabHttp/PlayFabUnityHttp.cs
+++ targets/unity-v2/source/ExampleTestProject/Assets/PlayFabSDK/Shared/Internal/PlayFabHttp/PlayFabUnityHttp.cs
@@ -97,7 +97,7 @@ namespace PlayFab.Internal
             CallRequestContainer reqContainer = (CallRequestContainer)reqContainerObj;
             reqContainer.RequestHeaders["Content-Type"] = "application/json";
 
-#if !UNITY_WSA && !UNITY_WP8 && !UNITY_WEBGL &&!UNITY_EDITOR_WIN &&!UNITY_STANDALONE_WIN &&!UNITY_WSA_10_0 // Unity has removed compression in Windows (will be added again at a later point)
+#if !UNITY_WSA && !UNITY_WP8 && !UNITY_WEBGL
             if (PlayFabSettings.CompressApiData)
             {
 #if UNITY_2021_3_ORHIGHER /// or whatever the actual one is

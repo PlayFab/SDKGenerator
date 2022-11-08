a92f87f6f56cd6756464b2ac94c492b7d9016319
Todd Bello <toddbell@microsoft.com>
2022-11-08 09:35:27 -0800

    Create makeTestTitleData.sh
    add a script that can output a secure testTitleData.json file

---
+++ SetupScripts/makeTestTitleData.sh
@@ -0,0 +1,21 @@
+# MakeTestTitleDataFile testTitleStartDirectory devSecretKey aliasId
+MakeTestTitleDataFile()
+{
+    if [ ! -d "$1" ]
+    then
+        echo === testTitleData directory Not Detected ===
+        echo ======== Creating ========
+        mkdir "$1"
+    fi
+
+    pushd "$1"
+    touch testTtileData.json
+    echo '{
+	"titleId": "6195",
+	"developerSecretKey": "$2",
+	"aliasId": "$3",
+	"userEmail": "a@b.com",
+	"connectionString": "https://6195.playfabapi.com"
+}' > testTtileData.json
+    popd
+}

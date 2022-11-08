86d37635ab03c32d5e521ebc7685415fca591b84
Todd Bello <toddbell@microsoft.com>
2022-11-08 10:55:11 -0800

    Update makeTestTitleData.sh
    add debug statements as this file seems to not load properly

--- SetupScripts/makeTestTitleData.sh
+++ SetupScripts/makeTestTitleData.sh
@@ -4,6 +4,7 @@ set -e
 # MakeTestTitleDataFile testTitleStartDirectory devSecretKey aliasId
 MakeTestTitleDataFile()
 {
+    echo === start call ====
     if [ ! -d $1 ]
     then
         echo === testTitleData directory Not Detected ===
@@ -12,13 +13,14 @@ MakeTestTitleDataFile()
     fi
 
     pushd $1
-    touch testTtileData.json
-    echo '{
-	"titleId": "6195",
-	"developerSecretKey": "$2",
-	"aliasId": "$3",
-	"userEmail": "a@b.com",
-	"connectionString": "https://6195.playfabapi.com"
-}' > testTtileData.json
+    touch testTitleData.json
+    echo "{
+	\"titleId\": \"6195\",
+	\"developerSecretKey\": \"$2\",
+	\"aliasId\": \"$3\",
+	\"userEmail\": \"a@b.com\",
+	\"connectionString\": \"https://6195.playfabapi.com\"
+}" > testTitleData.json
     popd
+    echo === end call ===
 }

b99dec3e41c9e9ec3667439e6e6c1611b4f4d2c9
Andy McCalib <andmcc@microsoft.com>
2023-09-21 17:01:00 -0700

    LuaSdk request/result alignment

--- targets/LuaSdk/make.js
+++ targets/LuaSdk/make.js
@@ -176,7 +176,7 @@ function getResultAction(tabbing, apiCall) {
     var internalTabbing = tabbing + "    ";
     if (apiCall.url === "/Authentication/GetEntityToken")
         preCallback = internalTabbing + "PlayFabSettings._internalSettings.entityToken = result.EntityToken\n";
-    else if (apiCall.result === "LoginResult" || apiCall.request === "RegisterPlayFabUserRequest") {
+    else if (apiCall.result === "LoginResult" || apiCall.result === "RegisterPlayFabUserResult") {
         preCallback = internalTabbing + "PlayFabSettings._internalSettings.sessionTicket = result.SessionTicket\n"
             + internalTabbing + "PlayFabSettings._internalSettings.entityToken = result.EntityToken.EntityToken\n";
     }

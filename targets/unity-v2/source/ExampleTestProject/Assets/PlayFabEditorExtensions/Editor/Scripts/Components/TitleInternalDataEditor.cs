4f913708aa8260f9aaa580d064eec12dc32313d1
Ashish Singh (HCL TECHNOLOGIES CORPORATE SER) <v-ashishsing@microsoft.com>
2024-10-01 01:21:07 +0530

    A11y additional  issue has been fixed

--- targets/unity-v2/source/ExampleTestProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Components/TitleInternalDataEditor.cs
+++ targets/unity-v2/source/ExampleTestProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Components/TitleInternalDataEditor.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design;
 using UnityEditor;
 using UnityEngine;
 
@@ -13,14 +14,55 @@ namespace PlayFab.PfEditor
 
         public Vector2 scrollPos = Vector2.zero;
 
+        //void OnGUI()
+        //{
+        //    // The actual window code goes here
+        //    using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1")))
+        //        EditorGUILayout.LabelField(string.Format("Testing: {0}", key), PlayFabEditorHelper.uiStyle.GetStyle("orTitle"), GUILayout.MinWidth(EditorGUIUtility.currentViewWidth));
+
+        //    scrollPos = GUILayout.BeginScrollView(scrollPos, PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1"));
+        //    Value = EditorGUILayout.TextArea(Value, PlayFabEditorHelper.uiStyle.GetStyle("editTxt"));
+        //    GUI.skin.settings.cursorColor = Color.black;
+        //    GUI.SetNextControlName("TextAreaInternal");
+        //    GUILayout.EndScrollView();
+
+
+        //    using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleClear")))
+        //    {
+        //        GUILayout.FlexibleSpace();
+        //        GUI.SetNextControlName("SaveButtonInternal");
+        //        if (GUILayout.Button("Save", PlayFabEditorHelper.uiStyle.GetStyle("Button"), GUILayout.MaxWidth(200)))
+        //        {
+        //            for (int z = 0; z < PlayFabEditorDataMenu.tdInternalViewer.items.Count; z++)
+        //            {
+        //                if (PlayFabEditorDataMenu.tdInternalViewer.items[z].Key == key)
+        //                {
+        //                    PlayFabEditorDataMenu.tdInternalViewer.items[z].Value = Value;
+        //                    PlayFabEditorDataMenu.tdInternalViewer.items[z].isDirty = true;
+        //                }
+        //            }
+        //            GUI.SetNextControlName("SaveButtonInternal");
+        //            Close();
+
+        //        }
+        //        GUILayout.FlexibleSpace();
+        //    }
+
+        //    Repaint();
+        //    GUI.SetNextControlName("SaveButtonInternal");
+        //}
+
         void OnGUI()
         {
             // The actual window code goes here
             using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1")))
+            {
                 EditorGUILayout.LabelField(string.Format("Editing: {0}", key), PlayFabEditorHelper.uiStyle.GetStyle("orTitle"), GUILayout.MinWidth(EditorGUIUtility.currentViewWidth));
 
+            }
             scrollPos = GUILayout.BeginScrollView(scrollPos, PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1"));
             Value = EditorGUILayout.TextArea(Value, PlayFabEditorHelper.uiStyle.GetStyle("editTxt"));
+            GUI.SetNextControlName("TextArea");
             GUI.skin.settings.cursorColor = Color.black;
             GUILayout.EndScrollView();
 
@@ -28,6 +70,7 @@ namespace PlayFab.PfEditor
             using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleClear")))
             {
                 GUILayout.FlexibleSpace();
+                GUI.SetNextControlName("SaveButton");
                 if (GUILayout.Button("Save", PlayFabEditorHelper.uiStyle.GetStyle("Button"), GUILayout.MaxWidth(200)))
                 {
                     for (int z = 0; z < PlayFabEditorDataMenu.tdInternalViewer.items.Count; z++)
@@ -38,6 +81,7 @@ namespace PlayFab.PfEditor
                             PlayFabEditorDataMenu.tdInternalViewer.items[z].isDirty = true;
                         }
                     }
+                    GUI.FocusControl("SaveButton");
                     Close();
 
                 }
@@ -45,7 +89,34 @@ namespace PlayFab.PfEditor
             }
 
             Repaint();
+            HandleFocusTrap();
         }
+        void HandleFocusTrap()
+        {
+            if (Event.current.type == EventType.KeyDown)
+            {
+                if (Event.current.control && Event.current.keyCode == KeyCode.Tab)
+                {
+                    if (GUI.GetNameOfFocusedControl() == "SaveButton")
+                    {
+                        GUI.FocusControl("TextArea");
+                        Event.current.Use();
+                    }
+                    else
+                    {
+                        GUI.FocusControl("SaveButton");
+                        Event.current.Use();
+                    }
+                }
+                else if((Event.current.keyCode == KeyCode.Escape))
+                {
+                    Close();
+                }
+            }
+        }
+
+
+        
 
         public void LoadData(string k, string v)
         {

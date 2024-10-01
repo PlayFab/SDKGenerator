08c5b1f48b956d615592ffab2b73c62cc1fe6adc
Ashish Singh (HCL TECHNOLOGIES CORPORATE SER) <v-ashishsing@microsoft.com>
2024-10-01 22:37:18 +0530

    removed commented code

--- targets/unity-v2/source/ExampleTestProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Components/TitleInternalDataEditor.cs
+++ targets/unity-v2/source/ExampleTestProject/Assets/PlayFabEditorExtensions/Editor/Scripts/Components/TitleInternalDataEditor.cs
@@ -14,44 +14,6 @@ namespace PlayFab.PfEditor
 
         public Vector2 scrollPos = Vector2.zero;
 
-        //void OnGUI()
-        //{
-        //    // The actual window code goes here
-        //    using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1")))
-        //        EditorGUILayout.LabelField(string.Format("Testing: {0}", key), PlayFabEditorHelper.uiStyle.GetStyle("orTitle"), GUILayout.MinWidth(EditorGUIUtility.currentViewWidth));
-
-        //    scrollPos = GUILayout.BeginScrollView(scrollPos, PlayFabEditorHelper.uiStyle.GetStyle("gpStyleGray1"));
-        //    Value = EditorGUILayout.TextArea(Value, PlayFabEditorHelper.uiStyle.GetStyle("editTxt"));
-        //    GUI.skin.settings.cursorColor = Color.black;
-        //    GUI.SetNextControlName("TextAreaInternal");
-        //    GUILayout.EndScrollView();
-
-
-        //    using (new UnityHorizontal(PlayFabEditorHelper.uiStyle.GetStyle("gpStyleClear")))
-        //    {
-        //        GUILayout.FlexibleSpace();
-        //        GUI.SetNextControlName("SaveButtonInternal");
-        //        if (GUILayout.Button("Save", PlayFabEditorHelper.uiStyle.GetStyle("Button"), GUILayout.MaxWidth(200)))
-        //        {
-        //            for (int z = 0; z < PlayFabEditorDataMenu.tdInternalViewer.items.Count; z++)
-        //            {
-        //                if (PlayFabEditorDataMenu.tdInternalViewer.items[z].Key == key)
-        //                {
-        //                    PlayFabEditorDataMenu.tdInternalViewer.items[z].Value = Value;
-        //                    PlayFabEditorDataMenu.tdInternalViewer.items[z].isDirty = true;
-        //                }
-        //            }
-        //            GUI.SetNextControlName("SaveButtonInternal");
-        //            Close();
-
-        //        }
-        //        GUILayout.FlexibleSpace();
-        //    }
-
-        //    Repaint();
-        //    GUI.SetNextControlName("SaveButtonInternal");
-        //}
-
         void OnGUI()
         {
             // The actual window code goes here
